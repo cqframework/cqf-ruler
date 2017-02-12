d9ff8730fbe125b301116821d2b7c155d65ed5c6
Chris Schuler <Chris Schuler>
2017-02-11 19:14:15 -0700

    Finished narrative generation for clinical reasoning resources

--- src/main/java/org/opencds/cqf/providers/NarrativeGenerator.java
+++ src/main/java/org/opencds/cqf/providers/NarrativeGenerator.java
@@ -2,7 +2,11 @@ package org.opencds.cqf.providers;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.narrative.CustomThymeleafNarrativeGenerator;
+import org.hl7.fhir.dstu3.model.DomainResource;
+import org.hl7.fhir.dstu3.model.Measure;
+import org.hl7.fhir.dstu3.model.Medication;
 import org.hl7.fhir.dstu3.model.PlanDefinition;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 
 import java.io.*;
 import java.nio.file.Path;
@@ -13,6 +17,7 @@ import java.nio.file.Paths;
  */
 public class NarrativeGenerator {
 
+    // args[0] == relative path to json resource -> i.e. measure/cms146.json
     public static void main(String[] args) {
         Path pathToResources = Paths.get("src/main/resources/narratives").toAbsolutePath();
         Path pathToProp = pathToResources.resolve("narrative.properties");
@@ -21,16 +26,18 @@ public class NarrativeGenerator {
         FhirContext ctx = FhirContext.forDstu3();
         ctx.setNarrativeGenerator(gen);
 
-        Path pathToResource = pathToResources.resolve("ZikaVirusIntervention.json");
+        // examples are here: src/main/resources/narratives/examples
+        if (args.length < 1) { throw new IllegalArgumentException("provide a file name..."); }
+        Path pathToResource = pathToResources.resolve("examples/" + args[0]);
+
         try {
-            PlanDefinition planDefinition = (PlanDefinition) ctx.newJsonParser().parseResource(new FileReader(pathToResource.toFile()));
-            // gen.generateNarrative(ctx, planDefinition, new Narrative());
-            String resource = ctx.newJsonParser().encodeResourceToString(planDefinition);
-            planDefinition = (PlanDefinition) ctx.newJsonParser().parseResource(resource);
+            IBaseResource res = ctx.newJsonParser().parseResource(new FileReader(pathToResource.toFile()));
+            String resource = ctx.newJsonParser()
+                    .encodeResourceToString(ctx.newJsonParser().parseResource(new FileReader(pathToResource.toFile())));
 
             try {
                 PrintWriter writer = new PrintWriter(new File(pathToResources.resolve("scratch.xml").toString()), "UTF-8");
-                writer.println(planDefinition.getText().getDiv().getValue());
+                writer.println(((DomainResource) ctx.newJsonParser().parseResource(resource)).getText().getDiv().getValue());
                 writer.println();
                 writer.close();
             } catch (UnsupportedEncodingException e) {
