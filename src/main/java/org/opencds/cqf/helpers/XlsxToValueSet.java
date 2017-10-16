9c2332b85a9807c508d15947e32dbf043d7b4e25
Chris Schuler <cschuler@myharmoniq.com>
2017-10-15 22:24:11 -0600

    Making the XlsxToValueSet converter more configurable - can specify start row, and column indexes on command line

--- src/main/java/org/opencds/cqf/helpers/XlsxToValueSet.java
+++ src/main/java/org/opencds/cqf/helpers/XlsxToValueSet.java
@@ -18,6 +18,21 @@ import java.util.Map;
 
 public class XlsxToValueSet {
 
+    // Default indexes and output directory path
+    private static boolean valueSet = true;
+    private static int startLine = 0;
+    private static int oidCol = 0;
+    private static int systemCol = 1;
+    private static int versionCol = 2;
+    private static int codeCol = 3;
+    private static int displayCol = 4;
+
+    private static int urlCol = 1;
+
+    private static String outDir = "src/main/resources/valuesets/";
+
+    private static final String validFlags = "-b (line to start conversion)\n-o (oid column index)\n-s (code system column index)\n-v (version column index)\n-c (code column index)\n-d (display column index)\n-u (url column index) only used for CodeSystem conversion";
+
     private static Map<String, ValueSet> valuesets = new HashMap<>();
     private static Map<String, CodeSystem> codeSystems = new HashMap<>();
 
@@ -36,28 +51,23 @@ public class XlsxToValueSet {
         codeSystems.put(oid, cs);
     }
 
-    private static String getNextCellString(Iterator<Cell> colIterator) {
-        if (colIterator.hasNext()) {
-            Cell temp = colIterator.next();
-            temp.setCellType(CellType.STRING);
-            return temp.getStringCellValue();
-        }
-        return "";
+    private static String getNextCellString(Cell cell) {
+        cell.setCellType(CellType.STRING);
+        return cell.getStringCellValue();
     }
 
-    // Format:
-    //   oid ... system ... version ... code ... display
-    private static void resolveRowVs(Iterator<Cell> colIterator) {
-        String oid = getNextCellString(colIterator);
+    // default format: oid ... system ... version ... code ... display
+    private static void resolveRowVs(Row row) {
+        String oid = getNextCellString(row.getCell(oidCol));
 
         if (!valuesets.containsKey(oid)) {
             populateOidVs(oid);
         }
 
-        String system = getNextCellString(colIterator);
-        String version = getNextCellString(colIterator);
-        String code = getNextCellString(colIterator);
-        String display = getNextCellString(colIterator);
+        String system = getNextCellString(row.getCell(systemCol));
+        String version = getNextCellString(row.getCell(versionCol));
+        String code = getNextCellString(row.getCell(codeCol));
+        String display = getNextCellString(row.getCell(displayCol));
 
         ValueSet.ValueSetComposeComponent vscc = valuesets.get(oid).getCompose();
         ValueSet.ConceptSetComponent component = new ValueSet.ConceptSetComponent();
@@ -66,18 +76,18 @@ public class XlsxToValueSet {
         vscc.addInclude(component);
     }
 
-    // oid ... url ... version ... code ... display
-    private static void resolveRowCs(Iterator<Cell> colIterator) {
-        String oid = getNextCellString(colIterator);
+    // default format: oid ... url ... version ... code ... display
+    private static void resolveRowCs(Row row) {
+        String oid = getNextCellString(row.getCell(oidCol));
 
         if (!codeSystems.containsKey(oid)) {
             populateOidCs(oid);
         }
 
-        String url = getNextCellString(colIterator);
-        String version = getNextCellString(colIterator);
-        String code = getNextCellString(colIterator);
-        String display = getNextCellString(colIterator);
+        String url = getNextCellString(row.getCell(urlCol));
+        String version = getNextCellString(row.getCell(versionCol));
+        String code = getNextCellString(row.getCell(codeCol));
+        String display = getNextCellString(row.getCell(displayCol));
 
         CodeSystem cs = codeSystems.get(oid);
         cs.setUrl(url).setVersion(version);
@@ -101,39 +111,46 @@ public class XlsxToValueSet {
     }
 
     // library function use
-    public static Bundle convertCs(Workbook workbook) {
+    public static Bundle convertCs(Workbook workbook, String[] args) {
+        resolveArgs(args);
         Iterator<Row> rowIterator = workbook.getSheetAt(0).iterator();
 
+        int currentRow = 0;
+        while (currentRow++ != startLine) {
+            rowIterator.next();
+        }
+
         while (rowIterator.hasNext()) {
-            Iterator<Cell> colIterator = rowIterator.next().iterator();
-            resolveRowCs(colIterator);
+            resolveRowCs(rowIterator.next());
         }
 
         return codesystemBundle();
     }
 
-    public static Bundle convertCs(String workbookPath) {
-        return convertCs(getWorkbook(workbookPath));
+    public static Bundle convertCs(String[] args) {
+        return convertCs(getWorkbook(args[0]), args);
     }
 
-    // library function use
-    public static Bundle convertVs(Workbook workbook) {
+    public static Bundle convertVs(Workbook workbook, String[] args) {
+        resolveArgs(args);
         Iterator<Row> rowIterator = workbook.getSheetAt(0).iterator();
 
+        int currentRow = 0;
+        while (currentRow++ != startLine) {
+            rowIterator.next();
+        }
+
         while (rowIterator.hasNext()) {
-            Iterator<Cell> colIterator = rowIterator.next().iterator();
-            resolveRowVs(colIterator);
+            resolveRowVs(rowIterator.next());
         }
 
         return valuesetBundle();
     }
 
-    // command line use
-    public static Bundle convertVs(String workbookPath) {
-        return convertVs(getWorkbook(workbookPath));
+    public static Bundle convertVs(String[] args) {
+        return convertVs(getWorkbook(args[0]), args);
     }
 
-    // command line use
     public static Workbook getWorkbook(String workbookPath) {
         Workbook workbook = null;
         try {
@@ -146,17 +163,43 @@ public class XlsxToValueSet {
         return workbook;
     }
 
-    // TODO - this needs work
+    private static void resolveArgs(String[] args) {
+        for (String arg : args) {
+            String[] flagAndValue = arg.split("=");
+
+            switch (flagAndValue[0]) {
+                case "-b": startLine = Integer.parseInt(flagAndValue[1]); break;
+                case "-o": oidCol = Integer.parseInt(flagAndValue[1]); break;
+                case "-s": systemCol = Integer.parseInt(flagAndValue[1]); break;
+                case "-v": versionCol = Integer.parseInt(flagAndValue[1]); break;
+                case "-c": codeCol = Integer.parseInt(flagAndValue[1]); break;
+                case "-d": displayCol = Integer.parseInt(flagAndValue[1]); break;
+                case "-u": urlCol = Integer.parseInt(flagAndValue[1]); break;
+                case "-outDir": outDir = flagAndValue[1]; break;
+                case "-cs": valueSet = false; break;
+                default:
+                    if (flagAndValue[0].startsWith("-")) {
+                        throw new IllegalArgumentException(String.format("Invalid flag: %s\n%s", flagAndValue[0], validFlags));
+                    }
+                    break;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("Path to excel file is required");
             return;
         }
 
-        Bundle temp = convertVs(args[0]);
+        if (args.length > 1) {
+            resolveArgs(args);
+        }
+
+        Bundle temp = valueSet ? convertVs(args) : convertCs(args);
         FhirContext context = FhirContext.forDstu3();
         for (Bundle.BundleEntryComponent component : temp.getEntry()) {
-            File f = new File("src/main/resources/valuesets/" + component.getResource().getId() + ".json");
+            File f = new File(outDir + component.getResource().getId() + ".json");
             if (f.createNewFile()) {
                 PrintWriter writer = new PrintWriter(f);
                 writer.println(context.newJsonParser().setPrettyPrint(true).encodeResourceToString(component.getResource()));
--- src/test/java/org/opencds/cqf/RulerHelperTests.java
+++ src/test/java/org/opencds/cqf/RulerHelperTests.java
@@ -21,41 +21,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 public class RulerHelperTests {
 
     // These tests were used for hedis terminology that we do not have permission to share yet.
     // Uncomment and add spreadsheets when permission is given, otherwise construct new tests.
-//    @Test
-//    public void XlsxToValueSetTest() {
-//        Bundle bundle = XlsxToValueSet.convertVs("src/test/resources/org/opencds/cqf/test_asf.xlsx");
-//        Assert.assertTrue(!bundle.getEntry().isEmpty());
-//        printBundleEntry(bundle);
-//
-//        bundle = XlsxToValueSet.convertVs("src/test/resources/org/opencds/cqf/test_dms-drr.xlsx");
-//        Assert.assertTrue(!bundle.getEntry().isEmpty());
-//        printBundleEntry(bundle);
-//
-//        bundle = XlsxToValueSet.convertVs("src/test/resources/org/opencds/cqf/test_dsf.xlsx");
-//        Assert.assertTrue(!bundle.getEntry().isEmpty());
-//        printBundleEntry(bundle);
-//
-//        bundle = XlsxToValueSet.convertVs("src/test/resources/org/opencds/cqf/test_pvc.xlsx");
-//        Assert.assertTrue(!bundle.getEntry().isEmpty());
-//        printBundleEntry(bundle);
-//
-//        bundle = XlsxToValueSet.convertCs("src/test/resources/org/opencds/cqf/test_all.xlsx");
-//        Assert.assertTrue(!bundle.getEntry().isEmpty());
-//        printBundleEntry(bundle);
-//    }
-//
-//    private void printBundleEntry(Bundle bundle) {
-//        for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
-//            System.out.println(FhirContext.forDstu3().newJsonParser().encodeResourceToString(entry.getResource()));
-//        }
-//    }
+    @Test
+    public void XlsxToValueSetTest() throws IOException {
+        String[] args = { "src/test/resources/org/opencds/cqf/test.xlsx", "-b=1", "-s=3", "-v=4", "-c=5", "-d=6"};
+        Bundle bundle = XlsxToValueSet.convertVs(args);
+        XlsxToValueSet.main(args);
+        Assert.assertTrue(!bundle.getEntry().isEmpty());
+
+        String[] csArgs = { "src/test/resources/org/opencds/cqf/test.xlsx", "-b=1", "-o=8", "-u=7", "-v=4", "-c=5", "-d=6", "-cs", "-outDir=src/main/resources/codesystems/"};
+        bundle = XlsxToValueSet.convertCs(csArgs);
+        XlsxToValueSet.main(csArgs);
+        Assert.assertTrue(!bundle.getEntry().isEmpty());
+    }
 
     @Test
     public void TestCOL() throws IOException, JAXBException {
