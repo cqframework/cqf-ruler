ae4d6d33636367d71711adad70a037f4036c6849
Chris Schuler <cschuler@myharmoniq.com>
2018-01-17 14:33:45 -0700

    Added new and extended existing builders according to Philips Healthcare update

--- src/main/java/org/opencds/cqf/builders/CodingBuilder.java
+++ src/main/java/org/opencds/cqf/builders/CodingBuilder.java
@@ -23,6 +23,19 @@ public class CodingBuilder extends BaseBuilder<Coding> {
         return this;
     }
 
+    public CodingBuilder buildCode(String system, String code, String display) {
+        complexProperty.setSystem(system);
+        complexProperty.setCode(code);
+        complexProperty.setDisplay(display);
+        return this;
+    }
+
+    public CodingBuilder buildCode(String system, String code) {
+        complexProperty.setSystem(system);
+        complexProperty.setCode(code);
+        return this;
+    }
+
     public CodingBuilder buildDisplay(String display) {
         complexProperty.setDisplay(display);
         return this;
