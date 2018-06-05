6f494038e4c5e4b3a1b58176d21698f0cbec393d
Chris Schuler <cschuler@myharmoniq.com>
2018-06-05 02:17:17 -0600

    Configured the MeasureSubscriptionServlet in web.xml so requests can be sent ... added my notes and alternative logic

--- src/main/java/org/opencds/cqf/servlet/MeasureSubscriptionServlet.java
+++ src/main/java/org/opencds/cqf/servlet/MeasureSubscriptionServlet.java
@@ -1,13 +1,9 @@
 package org.opencds.cqf.servlet;
 
 import ca.uhn.fhir.jpa.provider.dstu3.JpaResourceProviderDstu3;
-import ca.uhn.fhir.jpa.rp.dstu3.CodeSystemResourceProvider;
-import ca.uhn.fhir.jpa.rp.dstu3.ValueSetResourceProvider;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.hl7.fhir.dstu3.model.*;
-import org.opencds.cqf.cql.terminology.TerminologyProvider;
 import org.opencds.cqf.providers.JpaDataProvider;
-import org.opencds.cqf.providers.JpaTerminologyProvider;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,8 +21,9 @@ public class MeasureSubscriptionServlet extends BaseServlet {
     // NOTE: This ruler is not enabled for subscription, so we will fire the subscription notification manually
 
     // Issue the $evaluate-measure for the subscription endpoint
+    // $evaluate-measure?patient=Patient-1153&periodStart=2017-01&periodEnd=2017-12&lastUpdated=@lastUpdated
 
-    // $evaluate-measure?patient=Patient-1153&periodStart=2017-01&periodNed=2017-12&lastUpdated=@lastUpdated
+    // To hit this: POST <base>/cqf-ruler/on-measure-subscription-mrp
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: This endpoint assumes that we've registered the subscription here
         IGenericClient client = getFhirContext().newRestfulGenericClient("https://fhir-pit.mihin.org/cqf-ruler/baseDstu3");
@@ -50,4 +47,60 @@ public class MeasureSubscriptionServlet extends BaseServlet {
             resourceProvider.getDao().create(entry.getResource());
         }
     }
+
+    /*
+        Chris Schuler's comments:
+            I think I understand what the above POST request is trying to accomplish, but I think we can do better.
+            Why are we running the measure again only to get the evaluated resources to update this server? Or
+            is the subscribed-to server updating the resources and this endpoint merely re-runs the measure? If the
+            former question is true, then I think it is kind of ugly. If the latter, then disregard the rest of this.
+            From what I understand from reading this: http://hl7.org/fhir/subscription.html#2.46.6.1, we can
+            configure a much cleaner way in the subscription.
+
+            I am suggesting the Subscription resources look something like the following:
+                Subscription
+                    status: active
+                    criteria: ...
+                    channel:
+                        type: rest-hook
+                        endpoint: <base>/cqf-ruler/baseDstu3
+                        payload: application/fhir+json or application/fhir+xml
+                        header: ...
+
+            An example for the BCS measure (during 1997 measurement year) would be the following criteria
+            for the "Is Bilateral Mastectomy" expression:
+                Procedure?status=completed&date=eb1997-12-31&code:in=2.16.840.1.113883.3.464.1004.1042
+            We would need Subscriptions for all the data requirements in the library, which could be automated
+            using the $data-requirements operation.
+            With this Subscription the subscribed-to server could simply POST that resource (or a Bundle
+            of resources) to this endpoint.
+     */
+    /*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isJson = true;
+        if (request.getContentType().startsWith("application/fhir+xml")) {
+            isJson = false;
+        }
+        else if (!request.getContentType().startsWith("application/fhir+json")) {
+            throw new ServletException(String.format("Exprecting content type of application/fhir+json or application/fhir+xml, found %s.", request.getContentType()));
+        }
+
+        JpaDataProvider provider = new JpaDataProvider(getResourceProviders());
+        Resource resource = (Resource) (isJson
+                                ? provider.getFhirContext().newJsonParser().parseResource(request.getReader())
+                                : provider.getFhirContext().newXmlParser().parseResource(request.getReader()));
+
+        if (resource instanceof Bundle) {
+            for (Bundle.BundleEntryComponent entry : ((Bundle) resource).getEntry()) {
+                if (entry.hasResource()) {
+                    JpaResourceProviderDstu3 resourceProvider = provider.resolveResourceProvider(entry.fhirType());
+                    resourceProvider.getDao().update(entry.getResource());
+                }
+            }
+        }
+
+        else {
+            JpaResourceProviderDstu3 resourceProvider = provider.resolveResourceProvider(resource.fhirType());
+            resourceProvider.getDao().update(resource);
+        }
+    }*/
 }
