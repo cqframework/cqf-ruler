c5acc4d2ed5f56538b69e2dbdd5ff312a609bee7
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-03-07 23:02:41 -0700

    Fallback to embedded resource if unable to locate a library locally

--- src/main/java/org/opencds/cqf/config/STU3LibrarySourceProvider.java
+++ src/main/java/org/opencds/cqf/config/STU3LibrarySourceProvider.java
@@ -1,6 +1,8 @@
 package org.opencds.cqf.config;
 
 import ca.uhn.fhir.jpa.rp.dstu3.LibraryResourceProvider;
+
+import org.cqframework.cql.cql2elm.FhirLibrarySourceProvider;
 import org.cqframework.cql.cql2elm.LibrarySourceProvider;
 import org.hl7.elm.r1.VersionedIdentifier;
 import org.hl7.fhir.dstu3.model.IdType;
@@ -14,22 +16,32 @@ import java.io.InputStream;
 public class STU3LibrarySourceProvider implements LibrarySourceProvider {
 
     private LibraryResourceProvider provider;
+    private FhirLibrarySourceProvider innerProvider;
 
     public STU3LibrarySourceProvider(LibraryResourceProvider provider) {
         this.provider = provider;
+        this.innerProvider = new FhirLibrarySourceProvider();
     }
 
     @Override
     public InputStream getLibrarySource(VersionedIdentifier versionedIdentifier) {
         IdType id = new IdType(versionedIdentifier.getId());
-        org.hl7.fhir.dstu3.model.Library lib = provider.getDao().read(id);
-        for (org.hl7.fhir.dstu3.model.Attachment content : lib.getContent()) {
-            if (content.getContentType().equals("text/cql")) {
-                return new ByteArrayInputStream(content.getData());
+        org.hl7.fhir.dstu3.model.Library lib = null;
+        try {
+            lib = provider.getDao().read(id);
+        }
+        catch (Exception e) {
+
+        }
+
+        if (lib != null) {
+            for (org.hl7.fhir.dstu3.model.Attachment content : lib.getContent()) {
+                if (content.getContentType().equals("text/cql")) {
+                    return new ByteArrayInputStream(content.getData());
+                }
             }
         }
 
-        throw new IllegalArgumentException(String.format("Library %s%s does not contain CQL source content.", versionedIdentifier.getId(),
-                versionedIdentifier.getVersion() != null ? ("-" + versionedIdentifier.getVersion()) : ""));
+        return this.innerProvider.getLibrarySource(versionedIdentifier);
     }
 }
