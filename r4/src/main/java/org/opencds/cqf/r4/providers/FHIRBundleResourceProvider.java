65f50f4e75128d600d0e968c7bfd000db5b11e56
Jason Evans <jason@databaseconsultinggroup.com>
2019-09-14 11:54:15 -0400

    Revert "Update FHIRBundleResourceProvider.java"
    This reverts commit 99ab50b56165015a503fdcd0fd69227079e707a3.

--- r4/src/main/java/org/opencds/cqf/r4/providers/FHIRBundleResourceProvider.java
+++ r4/src/main/java/org/opencds/cqf/r4/providers/FHIRBundleResourceProvider.java
@@ -4,7 +4,7 @@ import ca.uhn.fhir.jpa.rp.r4.BundleResourceProvider;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
-import org.apache.commons.lang3.tuple.Pair;
+import javafx.util.Pair;
 import org.cqframework.cql.cql2elm.LibraryManager;
 import org.cqframework.cql.cql2elm.ModelManager;
 import org.cqframework.cql.elm.execution.Library;
