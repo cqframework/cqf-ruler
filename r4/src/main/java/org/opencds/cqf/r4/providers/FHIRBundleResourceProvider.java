7cbf09f7a84e3a1534a1624d2d79328fc369264b
Jason Evans <jason@databaseconsultinggroup.com>
2019-09-14 11:54:06 -0400

    Revert "Update FHIRBundleResourceProvider.java"
    This reverts commit b021010349e121be6b1e015dc456cc011fa43c33.

--- r4/src/main/java/org/opencds/cqf/r4/providers/FHIRBundleResourceProvider.java
+++ r4/src/main/java/org/opencds/cqf/r4/providers/FHIRBundleResourceProvider.java
@@ -4,7 +4,7 @@ import ca.uhn.fhir.jpa.rp.r4.BundleResourceProvider;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
-//import javafx.util.Pair;
+//import org.apache.commons.lang3.tuple.Pair;
 import org.cqframework.cql.cql2elm.LibraryManager;
 import org.cqframework.cql.cql2elm.ModelManager;
 import org.cqframework.cql.elm.execution.Library;
@@ -17,71 +17,14 @@ import org.opencds.cqf.r4.helpers.LibraryHelper;
 import java.math.BigDecimal;
 import java.util.*;
 
-class Pair<K,V> { //implements Serializable{
-
-    /**
-     * Key of this <code>Pair</code>.
-     */
-    private K key;
-
-    /**
-     * Gets the key for this pair.
-     * @return key for this pair
-     */
-    public K getKey() { return key; }
-
-    /**
-     * Value of this this <code>Pair</code>.
-     */
-    private V value;
-
-    /**
-     * Gets the value for this pair.
-     * @return value for this pair
-     */
-    public V getValue() { return value; }
-
-    /**
-     * Creates a new pair
-     * @param key The key for this pair
-     * @param value The value to use for this pair
-     */
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    /**
-     * <p><code>String</code> representation of this
-     * <code>Pair</code>.</p>
-     *
-     * <p>The default name/value delimiter '=' is always used.</p>
-     *
-     *  @return <code>String</code> representation of this <code>Pair</code>
-     */
-    @Override
-    public String toString() {
-        return key + "=" + value;
-    }
-
-    /**
-     * <p>Generate a hash code for this <code>Pair</code>.</p>
-     *
-     * <p>The hash code is calculated using both the name and
-     * the value of the <code>Pair</code>.</p>
-     *
-     * @return hash code for this <code>Pair</code>
-     */
-    @Override
-    public int hashCode() {
-        // name's hashCode is multiplied by an arbitrary prime number (13)
-        // in order to make sure there is a difference in the hashCode between
-        // these two parameters:
-        //  name: a  value: aa
-        //  name: aa value: a
-        return key.hashCode() * 13 + (value == null ? 0 : value.hashCode());
-    }
- }
+class Pair
+{
+	// Return a map entry (key-value pair) from the specified values
+	public static <T, U> Map.Entry<T, U> of(T first, U second)
+	{
+		return new AbstractMap.SimpleEntry<>(first, second);
+	}
+}
 
 public class FHIRBundleResourceProvider extends BundleResourceProvider {
 
@@ -150,12 +93,12 @@ public class FHIRBundleResourceProvider extends BundleResourceProvider {
                     if (((Element) childBase).hasExtension()) {
                         for (Extension extension : ((Element) childBase).getExtension()) {
                             if (extension.getUrl().equals("http://hl7.org/fhir/StructureDefinition/cqf-expression")) {
-                                return new Pair<String, String>(child.getName(), ((Expression) extension.getValue()).getExpression());
+                                return new Pair<>(child.getName(), ((Expression) extension.getValue()).getExpression());
                             }
                         }
                     }
                     else if (childBase instanceof Extension) {
-                        return new Pair<String, String>(child.getName(), ((Expression) ((Extension) childBase).getValue()).getExpression());
+                        return new Pair<>(child.getName(), ((Expression) ((Extension) childBase).getValue()).getExpression());
                     }
                 }
             }
