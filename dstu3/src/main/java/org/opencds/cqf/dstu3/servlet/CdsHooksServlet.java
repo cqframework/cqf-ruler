08c7111203a7de1596cd8b3eb96065f2a1cc2eb0
Adam Stevenson <stevenson_adam@yahoo.com>
2019-09-16 17:34:16 -0600

    Fixed server baseUrl in CDS post.

--- dstu3/src/main/java/org/opencds/cqf/dstu3/servlet/CdsHooksServlet.java
+++ dstu3/src/main/java/org/opencds/cqf/dstu3/servlet/CdsHooksServlet.java
@@ -93,7 +93,7 @@ public class CdsHooksServlet extends HttpServlet
 
             String baseUrl =
                     request.getRequestURL().toString()
-                            .replace(request.getPathInfo(), "").replace(request.getServletPath(), "") + "/baseDstu3";
+                            .replace(request.getPathInfo(), "").replace(request.getServletPath(), "") + "/fhir";
             String service = request.getPathInfo().replace("/", "");
 
             JsonParser parser = new JsonParser();
