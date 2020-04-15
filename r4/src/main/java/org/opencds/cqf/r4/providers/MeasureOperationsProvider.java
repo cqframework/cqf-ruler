e022a3c3220a57e9fd0819b8caa1e65d1c9fd324
Brenin <barhodes@myharmoniq.com>
2020-04-15 09:59:24 -0600

    Related artifacts in the measure resource are now cleared completely during refreshGeneratedContent to avoid leaving artifacts that are no longer referenced.

--- r4/src/main/java/org/opencds/cqf/r4/providers/MeasureOperationsProvider.java
+++ r4/src/main/java/org/opencds/cqf/r4/providers/MeasureOperationsProvider.java
@@ -93,6 +93,9 @@ public class MeasureOperationsProvider {
     public MethodOutcome refreshGeneratedContent(HttpServletRequest theRequest, RequestDetails theRequestDetails,
             @IdParam IdType theId) {
         Measure theResource = this.measureResourceProvider.getDao().read(theId);
+
+        theResource.getRelatedArtifact().clear();
+
         CqfMeasure cqfMeasure = this.dataRequirementsProvider.createCqfMeasure(theResource, this.libraryResolutionProvider);
 
         // Ensure All Related Artifacts for all referenced Libraries
