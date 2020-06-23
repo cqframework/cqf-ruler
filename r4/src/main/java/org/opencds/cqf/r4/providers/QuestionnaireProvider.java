75a9c7917e232969ab2b9bfd53f1210bc3472186
bryant <bryant@alphora.com>
2020-06-23 09:08:02 -0600

    convert questionnaireResponse to Observation

--- r4/src/main/java/org/opencds/cqf/r4/providers/QuestionnaireProvider.java
+++ r4/src/main/java/org/opencds/cqf/r4/providers/QuestionnaireProvider.java
@@ -1,19 +1,13 @@
 package org.opencds.cqf.r4.providers;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import ca.uhn.fhir.util.BundleUtil;
-import com.github.dnault.xmlpatch.repackaged.org.jaxen.util.SingletonList;
-import org.hl7.fhir.DateTime;
 import org.hl7.fhir.r4.model.*;
 import org.opencds.cqf.common.config.HapiProperties;
-import org.opencds.cqf.utilities.BundleUtils;
 
 import java.util.Date;
-import java.util.List;
 
 import static org.opencds.cqf.common.helpers.ClientHelper.getClient;
 
@@ -26,41 +20,49 @@ public class QuestionnaireProvider {
 
     @Operation(name = "$extract", idempotent = false, type = QuestionnaireResponse.class)
     public Bundle extractObservationFromQuestionnaireResponse(@OperationParam(name = "questionnaireResponse") QuestionnaireResponse questionnaireResponse) {
-        Bundle observationsFromQuestionnaireResponse = createObservationBundle(questionnaireResponse);
-        Bundle returnBundle = sendObservationBundle(observationsFromQuestionnaireResponse);
-        return returnBundle;
+        if(questionnaireResponse != null) {
+            Bundle observationsFromQuestionnaireResponse = createObservationBundle(questionnaireResponse);
+            Bundle returnBundle = sendObservationBundle(observationsFromQuestionnaireResponse);
+            return returnBundle;
+        }
+        return null;
     }
 
     private Bundle createObservationBundle(QuestionnaireResponse questionnaireResponse){
         Bundle newBundle = new Bundle();
+        Date authored = questionnaireResponse.getAuthored();
 
         Identifier bundleId = new Identifier();
-        bundleId.setValue(questionnaireResponse.getId());
+        bundleId.setValue("QuestionnaireResponse/" + questionnaireResponse.getIdElement().getIdPart());
         newBundle.setType(Bundle.BundleType.TRANSACTION);
         newBundle.setIdentifier(bundleId);
 
         questionnaireResponse.getItem().stream().forEach(item ->{
-            newBundle.addEntry(extractItem(item, questionnaireResponse));
+            newBundle.addEntry(extractItem(item, authored, questionnaireResponse));
         });
         return newBundle;
     }
 
-    //TODO - ids need work; add encounter; subject/patient; effectiveDateTime; value
-    // TODO - using value for questionnaire question - NOT THE RIGHT PLACE!!
-    private Bundle.BundleEntryComponent extractItem(QuestionnaireResponse.QuestionnaireResponseItemComponent item, QuestionnaireResponse questionnaireResponse){
-        //       obs.setCode();
-//        obs.setValue() =
-
+    private Bundle.BundleEntryComponent extractItem(QuestionnaireResponse.QuestionnaireResponseItemComponent item, Date authored, QuestionnaireResponse questionnaireResponse){
         Observation obs = new Observation();
+        obs.setEffective(new DateTimeType(authored));
         obs.setStatus(Observation.ObservationStatus.FINAL);
-        obs.setId(item.getLinkId());
-//        obs.setDerivedFrom(new SingletonList(questionnaireResponse.get);  //create a reference to the QuestionnaireResponse  ????
-        obs.setValue(new StringType(item.getText() + "::" + item.getAnswer().get(0).getValueStringType().getValue()));
-        obs.setEffective(new DateTimeType( questionnaireResponse.getMeta().getLastUpdated()));
-
+        Coding qrCoding = new Coding();
+        qrCoding.setCode("74465-6");
+        qrCoding.setDisplay(" Questionnaire response Document");
+        obs.setCode(new CodeableConcept().addCoding(qrCoding));
+        obs.setId(questionnaireResponse.getIdElement().getIdPart() + "." + item.getLinkId());
+        switch(item.getAnswer().get(0).getValue().fhirType()){
+            case "string":
+                obs.setValue(new StringType(item.getAnswer().get(0).getValueStringType().getValue()));
+                break;
+            case "Coding":
+                obs.setValue(new CodeableConcept().addCoding(item.getAnswer().get(0).getValueCoding()));
+                break;
+        }
         Bundle.BundleEntryRequestComponent berc = new Bundle.BundleEntryRequestComponent();
         berc.setMethod(Bundle.HTTPVerb.PUT);
-        berc.setUrl("Observation/" + item.getId() + "." + item.getLinkId());
+        berc.setUrl("Observation/" + obs.getId());
 
         Bundle.BundleEntryComponent bec = new Bundle.BundleEntryComponent();
         bec.setResource(obs);
@@ -73,47 +75,11 @@ public class QuestionnaireProvider {
         String user = HapiProperties.getObservationUserName();
         String password = HapiProperties.getObservationPassword();
 
+System.out.println(fhirContext.newJsonParser().encodeResourceToString(observationsBundle));
         IGenericClient client = getClient(fhirContext, url, user, password);
         Bundle outcomeBundle = client.transaction()
                 .withBundle(observationsBundle)
                 .execute();
         return outcomeBundle;
     }
-
-    /**
-     * How do we store the QuestionnaireResponse Id??
-     *
-     *     var obxs = [];
-     *     for(var i=0, iLen=values.length; i<iLen; i++) {
-     *       var obx = {
-     *         "resourceType": "Observation",
-     *         "status": "final",
-     *         "code": {
-     *           "coding": item.codeList,
-     *           "text": item.question
-     *         },
-     *
-     *         "encounter":"",//reference - QuestionnaireResponse  or Questionnaire https://www.hl7.org/fhir/valueset-resource-types.html
-     *         "effective": USE "meta": {
-     *           "lastUpdated": "2020-05-14T08:33:31.845-06:00",
-     *         },
-     *
-     *
-     *
-     *
-     *       };
-     *       this._addVersionTag(obx);
-     *       if (setId) {
-     *         obx.id = this._getUniqueId(item.questionCode);
-     *       }
-     *       if (!item.header) {
-     *         obx[values[i].key] = values[i].val;
-     *       }
-     *       obxs.push(obx);
-     *     }
-     *     return obxs;
-     */
-
-
-
 }
