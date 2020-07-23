7c9de3e17ff448e752b7d31843aa7ea3b5fa368c
bryant <bryant@alphora.com>
2020-07-23 12:11:04 -0600

    changes to Observation to facilitate use in PainManager

--- r4/src/main/java/org/opencds/cqf/r4/providers/QuestionnaireProvider.java
+++ r4/src/main/java/org/opencds/cqf/r4/providers/QuestionnaireProvider.java
@@ -7,6 +7,7 @@ import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.hl7.fhir.r4.model.*;
 import org.opencds.cqf.common.config.HapiProperties;
 
+import java.util.Collections;
 import java.util.Date;
 
 import static org.opencds.cqf.common.helpers.ClientHelper.getClient;
@@ -48,6 +49,10 @@ public class QuestionnaireProvider {
         obs.setEffective(new DateTimeType(authored));
         obs.setStatus(Observation.ObservationStatus.FINAL);
         obs.setSubject(questionnaireResponse.getSubject());
+        Coding qrCategoryCoding = new Coding();
+        qrCategoryCoding.setCode("survey");
+        qrCategoryCoding.setSystem("http://hl7.org/fhir/observation-category");
+        obs.setCategory(Collections.singletonList(new CodeableConcept().addCoding(qrCategoryCoding)));
         Coding qrCoding = new Coding();
         qrCoding.setCode("74465-6");
         qrCoding.setDisplay("Questionnaire response Document");
@@ -60,7 +65,24 @@ public class QuestionnaireProvider {
             case "Coding":
                 obs.setValue(new CodeableConcept().addCoding(item.getAnswer().get(0).getValueCoding()));
                 break;
+            case "boolean":
+                obs.setValue(new BooleanType(item.getAnswer().get(0).getValueBooleanType().booleanValue()));
+                break;
         }
+        Reference questionnaireResponseReference = new Reference();
+        questionnaireResponseReference.setReference("QuestionnaireResponse" + "/" + questionnaireResponse.getIdElement().getIdPart());
+        obs.setDerivedFrom(Collections.singletonList(questionnaireResponseReference));
+        Extension linkIdExtension = new Extension();
+        linkIdExtension.setUrl("http://hl7.org/fhir/uv/sdc/StructureDefinition/derivedFromLinkId");
+        linkIdExtension.setValue(new StringType(item.getLinkId()));
+//   Extension refExtension = new Extension();
+//        refExtension.setUrl(ExtensionEnum.GRCONTAINED.getUrl());
+//        if(!mReport.hasId()){
+//            mReport.setId(UUID.randomUUID().toString());
+//        }
+//        refExtension.setValue(new Reference("#" + mReport.getId()));
+//        newGuidanceResponse.addExtension(refExtension);        obs.
+        obs.addExtension(linkIdExtension);
         Bundle.BundleEntryRequestComponent berc = new Bundle.BundleEntryRequestComponent();
         berc.setMethod(Bundle.HTTPVerb.PUT);
         berc.setUrl("Observation/" + obs.getId());
