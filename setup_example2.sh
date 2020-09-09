ef14fa1123c3d5fa891455b656626c679a3abe4f
mdube <mdube@mitre.org>
2020-09-09 15:23:27 -0400

    Remove extra example

--- setup_example2.sh
+++ setup_example2.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # Post Bundle
-curl -s -X POST -H 'Content-Type: application/fhir+json' -d @example_plandef_bundle.json http://localhost:8080/cqf-ruler-r4/fhir
 curl -s -X POST -H 'Content-Type: application/fhir+json' -d @example_nested_plandef.json http://localhost:8080/cqf-ruler-r4/fhir
