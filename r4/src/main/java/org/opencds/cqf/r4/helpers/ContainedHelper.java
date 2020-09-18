1733e437bd6982e8b931c1b8b92df70a1940304d
Reece Adamson <radamson@mitre.org>
2020-09-18 12:35:21 -0400

    javadoc

--- r4/src/main/java/org/opencds/cqf/r4/helpers/ContainedHelper.java
+++ r4/src/main/java/org/opencds/cqf/r4/helpers/ContainedHelper.java
@@ -8,10 +8,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * This class consists exclusively of static methods that assist with managing contained FHIR
+ * Resources.
+ *
+ * The FHIR specification does not allow contained resources to contain additional resources:
+ * > Contained resources SHALL NOT contain additional contained resources.
+ * https://www.hl7.org/fhir/references.html#contained
+ *
+ * When returning a resource from an annotated method that responds to an incoming request any
+ * resources contained in another resource will be removed.  `liftContainedResourcesToParent` in
+ * this class will move all contained resources to the parent so they are present in responses
+ * to clients.
+ */
 public class ContainedHelper {
 
-  // > Contained resources SHALL NOT contain additional contained resources.
-  // https://www.hl7.org/fhir/references.html#contained
+  /**
+   * Adds all contained resources in resources contained on the parent to the parent.
+   *
+   * @param resource the parent resource where contained resources should be
+   * @return the modified parent resource
+   */
   public static DomainResource liftContainedResourcesToParent(DomainResource resource) {
     getContainedResourcesInContainedResources(resource)
         .forEach(resource::addContained);   // add them to the parent
@@ -19,6 +36,12 @@ public class ContainedHelper {
     return resource;  // Return the resource to allow for method chaining
   }
 
+  /**
+   * Returns all contained resources that are not already directly present on the parent resource.
+   *
+   * @param resource the parent resource
+   * @return list of the contained resources
+   */
   private static List<Resource> getContainedResourcesInContainedResources(Resource resource) {
     if (!(resource instanceof DomainResource)) {
       return new ArrayList<>();
@@ -26,6 +49,12 @@ public class ContainedHelper {
     return streamContainedResourcesInContainedResources(resource).collect(Collectors.toList());
   }
 
+  /**
+   * Returns all contained resources, including resources directly contained on the parent and
+   * resources contained on any other contained resource
+   * @param resource the parent resource
+   * @return list of all contained resources
+   */
   public static List<Resource> getAllContainedResources(Resource resource) {
     if (!(resource instanceof DomainResource)) {
       return new ArrayList<>();
@@ -33,6 +62,9 @@ public class ContainedHelper {
     return streamAllContainedResources(resource).collect(Collectors.toList());
   }
 
+  /**
+   * @see ContainedHelper#streamContainedResourcesInContainedResources(Resource)
+   */
   private static Stream<Resource> streamContainedResourcesInContainedResources(Resource resource) {
     if (!(resource instanceof DomainResource)) {
       return Stream.empty();
@@ -43,6 +75,9 @@ public class ContainedHelper {
         .flatMap(ContainedHelper::streamAllContainedResources);  // Get the resources contained
   }
 
+  /**
+   * @see ContainedHelper#streamAllContainedResources(Resource) 
+   */
   private static Stream<Resource> streamAllContainedResources(Resource resource) {
     if (!(resource instanceof DomainResource)) {
       return Stream.empty();
