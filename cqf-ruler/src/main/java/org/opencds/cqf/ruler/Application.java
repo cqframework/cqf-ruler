11d3ca9d7abaf6287cc58fa3d4bd363587d1b092
Jonathan Percival <jonathan.i.percival@gmail.com>
2021-05-27 12:41:38 -0600

    WIP updating to latest HAPI

--- cqf-ruler/src/main/java/org/opencds/cqf/ruler/Application.java
+++ cqf-ruler/src/main/java/org/opencds/cqf/ruler/Application.java
@@ -1,8 +1,11 @@
 package org.opencds.cqf.ruler;
 
-public class Application extends ca.uhn.fhir.jpa.starter.Application {
+public class Application {
 
   public static void main(String[] args) {
     ca.uhn.fhir.jpa.starter.Application.main(args);
+
+    //Server is now accessible at eg. http://localhost:8080/fhir/metadata
+    //UI is now accessible at http://localhost:8080/
   }
 }
