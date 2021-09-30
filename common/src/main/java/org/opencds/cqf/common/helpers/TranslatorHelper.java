da3381ac305a5721b37caef28a599d98e4fd5c8d
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2021-09-30 14:45:54 -0600

    use same translator everywhere

--- common/src/main/java/org/opencds/cqf/common/helpers/TranslatorHelper.java
+++ common/src/main/java/org/opencds/cqf/common/helpers/TranslatorHelper.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
@@ -48,16 +49,21 @@ public class TranslatorHelper {
                 modelManager);
     }
 
-    public static CqlTranslator getTranslator(InputStream cqlStream, LibraryManager libraryManager,
-            ModelManager modelManager) {
+    public static List<CqlTranslator.Options> generateDefaultTranslatorOptions() {
         ArrayList<CqlTranslator.Options> options = new ArrayList<>();
         options.add(CqlTranslator.Options.EnableAnnotations);
         options.add(CqlTranslator.Options.EnableLocators);
         options.add(CqlTranslator.Options.DisableListDemotion);
         options.add(CqlTranslator.Options.DisableListPromotion);
-        options.add(CqlTranslator.Options.DisableMethodInvocation);
+        return options;
+    }
+
+    public static CqlTranslator getTranslator(InputStream cqlStream, LibraryManager libraryManager,
+            ModelManager modelManager) {
+
         CqlTranslator translator;
         try {
+            List<CqlTranslator.Options> options = generateDefaultTranslatorOptions();
             translator = CqlTranslator.fromStream(cqlStream, modelManager, libraryManager,
                     options.toArray(new CqlTranslator.Options[options.size()]));
         } catch (IOException e) {
@@ -85,8 +91,7 @@ public class TranslatorHelper {
 
     public static CqlTranslatorOptions getTranslatorOptions() {
         CqlTranslatorOptions cqlTranslatorOptions = new CqlTranslatorOptions();
-        cqlTranslatorOptions.getFormats().add(CqlTranslator.Format.JSON);
-        cqlTranslatorOptions.getOptions().add(CqlTranslator.Options.EnableAnnotations);
+        cqlTranslatorOptions.getOptions().addAll(generateDefaultTranslatorOptions());
         cqlTranslatorOptions.setAnalyzeDataRequirements(true);
         cqlTranslatorOptions.setCollapseDataRequirements(true);
         return cqlTranslatorOptions;
