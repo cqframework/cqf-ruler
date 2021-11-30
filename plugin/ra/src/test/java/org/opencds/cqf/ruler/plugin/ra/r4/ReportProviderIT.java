754be1b6e8739a88a9ea532f2eb2725f80b9822e
rob-reynolds <rob@alphora.com>
2021-11-30 10:15:17 -0700

    Added auto organize of imports on save

--- plugin/ra/src/test/java/org/opencds/cqf/ruler/plugin/ra/r4/ReportProviderIT.java
+++ plugin/ra/src/test/java/org/opencds/cqf/ruler/plugin/ra/r4/ReportProviderIT.java
@@ -1,9 +1,9 @@
 package org.opencds.cqf.ruler.plugin.ra.r4;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.io.IOException;
 
--- plugin/test-utility/src/test/java/org/opencds/cqf/ruler/plugin/testutility/ClientUtilitiesTest.java
+++ plugin/test-utility/src/test/java/org/opencds/cqf/ruler/plugin/testutility/ClientUtilitiesTest.java
@@ -1,9 +1,9 @@
 package org.opencds.cqf.ruler.plugin.testutility;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.commons.validator.routines.UrlValidator;
-
 import org.junit.jupiter.api.Test;
 
 public class ClientUtilitiesTest implements ClientUtilities {
