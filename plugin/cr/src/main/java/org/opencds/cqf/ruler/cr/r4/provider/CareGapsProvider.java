59bb445b8fec9c17e0502022fb4d54a28f7ab01e
rob-reynolds <rob@alphora.com>
2022-01-29 18:16:19 -0700

    WIP: initial functional stub of $care-gaps operation

---
+++ plugin/cr/src/main/java/org/opencds/cqf/ruler/cr/r4/provider/CareGapsProvider.java
@@ -0,0 +1,92 @@
+package org.opencds.cqf.ruler.cr.r4.provider;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.hl7.fhir.r4.model.CanonicalType;
+import org.hl7.fhir.r4.model.Measure;
+import org.hl7.fhir.r4.model.Parameters;
+import org.opencds.cqf.ruler.provider.DaoRegistryOperationProvider;
+
+import ca.uhn.fhir.model.api.annotation.Description;
+import ca.uhn.fhir.rest.annotation.Operation;
+import ca.uhn.fhir.rest.annotation.OperationParam;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+
+public class CareGapsProvider extends DaoRegistryOperationProvider {
+
+	/**
+	 * Implements the <a href=
+	 * "http://build.fhir.org/ig/HL7/davinci-deqm/OperationDefinition-care-gaps.html">$care-gaps</a>
+	 * operation found in the
+	 * <a href="http://build.fhir.org/ig/HL7/davinci-deqm/index.html">Da Vinci DEQM
+	 * FHIR Implementation Guide</a> that overrides the <a href=
+	 * "http://build.fhir.org/operation-measure-care-gaps.html">$care-gaps</a>
+	 * operation found in the
+	 * <a href="http://hl7.org/fhir/R4/clinicalreasoning-module.html">FHIR Clinical
+	 * Reasoning Module</a>.
+	 * 
+	 * The operation calculates measures describing gaps in care. For more details,
+	 * reference the <a href=
+	 * "http://build.fhir.org/ig/HL7/davinci-deqm/gaps-in-care-reporting.html">Gaps
+	 * in Care Reporting</a> section of the
+	 * <a href="http://build.fhir.org/ig/HL7/davinci-deqm/index.html">Da Vinci DEQM
+	 * FHIR Implementation Guide</a>.
+	 * 
+	 * A Parameters resource that includes zero to many document bundles that
+	 * include Care Gap Measure Reports will be returned.
+	 * 
+	 * Usage:
+	 * URL: [base]/Measure/$care-gaps
+	 * 
+	 * @param theRequestDetails generally auto-populated by the HAPI server
+	 *                          framework.
+	 * @param periodStart       the start of the gaps through period
+	 * @param periodEnd         the end of the gaps through period
+	 * @param topic             the category of the measures that is of interest for
+	 *                          the care gaps report
+	 * @param subject           a reference to either a Patient or Group for which
+	 *                          the gaps in care report(s) will be generated
+	 * @param practitioner      a reference to a Practitioner for which the gaps in
+	 *                          care report(s) will be generated
+	 * @param organization      a reference to an Organization for which the gaps in
+	 *                          care report(s) will be generated
+	 * @param status            the status code of gaps in care reports that will be
+	 *                          included in the result
+	 * @param measureId         the id of Measure(s) for which the gaps in care
+	 *                          report(s) will be calculated
+	 * @param measureIdentifier the identifier of Measure(s) for which the gaps in
+	 *                          care report(s) will be calculated
+	 * @param measureUrl        the canonical URL of Measure(s) for which the gaps
+	 *                          in care report(s) will be calculated
+	 * @param program           the program that a provider (either clinician or
+	 *                          clinical organization) participates in
+	 * @return Parameters of bundles of Care Gap Measure Reports
+	 */
+	@Description(shortDefinition = "$care-gaps", value = "Implements the <a href=\"http://build.fhir.org/ig/HL7/davinci-deqm/OperationDefinition-care-gaps.html\">$care-gaps</a> operation found in the <a href=\"http://build.fhir.org/ig/HL7/davinci-deqm/index.html\">Da Vinci DEQM FHIR Implementation Guide</a> which is an extension of the <a href=\"http://build.fhir.org/operation-measure-care-gaps.html\">$care-gaps</a> operation found in the <a href=\"http://hl7.org/fhir/R4/clinicalreasoning-module.html\">FHIR Clinical Reasoning Module</a>.")
+	@Operation(name = "$care-gaps", idempotent = true, type = Measure.class)
+	public Parameters careGapsReport(RequestDetails theRequestDetails,
+			@OperationParam(name = "periodStart", min = 1, max = 1) String periodStart,
+			@OperationParam(name = "periodEnd", min = 1, max = 1) String periodEnd,
+			@OperationParam(name = "topic", min = 0) List<String> topic,
+			@OperationParam(name = "subject", min = 0, max = 1) String subject,
+			@OperationParam(name = "practitioner", min = 0, max = 1) String practitioner,
+			@OperationParam(name = "organization", min = 0, max = 1) String organization,
+			@OperationParam(name = "status", min = 1) List<String> status,
+			@OperationParam(name = "measureId", min = 0) List<String> measureId,
+			@OperationParam(name = "measureIdentifier", min = 0) List<String> measureIdentifier,
+			@OperationParam(name = "measureUrl", min = 0) List<CanonicalType> measureUrl,
+			@OperationParam(name = "program", min = 0) List<String> program) {
+
+		/*
+		 * TODO - topic should allow many and be a union of them
+		 * TODO -
+		 * "The Server needs to make sure that practitioner is authorized to get the gaps in care report for and know what measures the practitioner are eligible or qualified."
+		 */
+
+		Parameters resultParameters = new Parameters();
+		resultParameters.setId((UUID.randomUUID().toString()));
+
+		return resultParameters;
+	}
+}
