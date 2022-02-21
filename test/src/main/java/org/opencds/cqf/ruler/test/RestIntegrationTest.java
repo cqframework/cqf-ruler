a4351b81abf7bec421c3420260208990cee23ced
Jonathan Percival <jonathan.i.percival@gmail.com>
2022-02-21 16:27:36 -0700

    Restore cds-hooks tests

--- test/src/main/java/org/opencds/cqf/ruler/test/RestIntegrationTest.java
+++ test/src/main/java/org/opencds/cqf/ruler/test/RestIntegrationTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.opencds.cqf.ruler.Application;
 import org.opencds.cqf.ruler.behavior.IdCreator;
 import org.opencds.cqf.ruler.behavior.ResourceCreator;
+import org.opencds.cqf.ruler.external.AppProperties;
 import org.opencds.cqf.ruler.test.behavior.ResourceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -25,20 +26,22 @@ import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 
-
 @Import(Application.class)
 @TestPropertySource(properties = {
-	"scheduling_disabled=true",
-	"spring.main.allow-bean-definition-overriding=true",
-	"spring.batch.job.enabled=false",
-	"hapi.fhir.allow_external_references=true",
-	"hapi.fhir.enforce_referential_integrity_on_write=false",
-	"hapi.fhir.auto_create_placeholder_reference_targets=true",
-	"hapi.fhir.client_id_strategy=ANY",
-	"spring.datasource.url=jdbc:h2:mem:db",
-	"spring.main.lazy-initialization=true" })
+		"scheduling_disabled=true",
+		"spring.main.allow-bean-definition-overriding=true",
+		"spring.batch.job.enabled=false",
+		"hapi.fhir.allow_external_references=true",
+		"hapi.fhir.enforce_referential_integrity_on_write=false",
+		"hapi.fhir.auto_create_placeholder_reference_targets=true",
+		"hapi.fhir.client_id_strategy=ANY",
+		"spring.datasource.url=jdbc:h2:mem:db",
+		"spring.main.lazy-initialization=true" })
 @TestInstance(Lifecycle.PER_CLASS)
 public class RestIntegrationTest implements ResourceLoader, ResourceCreator, IdCreator {
+
+	@Autowired
+	AppProperties myAppProperties;
 
 	@Autowired
 	TestDbService myDbService;
@@ -54,6 +57,8 @@ public class RestIntegrationTest implements ResourceLoader, ResourceCreator, IdC
 
 	private IGenericClient myClient;
 
+	private String myServerBase;
+
 	@Override
 	public FhirContext getFhirContext() {
 		return myCtx;
@@ -64,6 +69,10 @@ public class RestIntegrationTest implements ResourceLoader, ResourceCreator, IdC
 		return myDaoRegistry;
 	}
 
+	protected String getServerBase() {
+		return myServerBase;
+	}
+
 	protected IGenericClient getClient() {
 		return myClient;
 	}
@@ -72,23 +81,28 @@ public class RestIntegrationTest implements ResourceLoader, ResourceCreator, IdC
 		return myPort;
 	}
 
+	protected AppProperties getAppProperties() {
+		return myAppProperties;
+	}
+
 	protected void stubResourceForUrl(String location, String url) throws IOException {
 		String resourceString = stringFromResource(location);
 		stubFor(
-        get(urlEqualTo(url))
-            .willReturn(
-                aResponse()
-                    .withStatus(200)
-                    .withBody(resourceString)
-                    .withHeader("Content-Type", "application/fhir+json; charset=utf-8")));
+				get(urlEqualTo(url))
+						.willReturn(
+								aResponse()
+										.withStatus(200)
+										.withBody(resourceString)
+										.withHeader("Content-Type", "application/fhir+json; charset=utf-8")));
 	}
 
 	@BeforeEach
 	void baseBeforeEach() {
 		myCtx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
 		myCtx.getRestfulClientFactory().setSocketTimeout(1200 * 1000);
-		String ourServerBase = "http://localhost:" + myPort + "/fhir/";
-		myClient = myCtx.newRestfulGenericClient(ourServerBase);
+		myServerBase = "http://localhost:" + myPort + "/fhir/";
+		myAppProperties.setServer_address(myServerBase);
+		myClient = myCtx.newRestfulGenericClient(myServerBase);
 	}
 
 	@AfterAll
