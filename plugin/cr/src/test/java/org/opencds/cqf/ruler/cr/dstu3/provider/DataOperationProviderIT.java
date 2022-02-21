47008d521ef963ad0d9735afc9bbc69d7cf72911
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2022-02-21 16:02:48 -0700

    fix filename in tests

--- plugin/cr/src/test/java/org/opencds/cqf/ruler/cr/dstu3/provider/DataOperationProviderIT.java
+++ plugin/cr/src/test/java/org/opencds/cqf/ruler/cr/dstu3/provider/DataOperationProviderIT.java
@@ -22,7 +22,7 @@ public class DataOperationProviderIT extends RestIntegrationTest {
 
 	@Test
 	public void testDstu3DataRequirementsOperation() throws IOException {
-		String bundleAsText = stringFromResource( "DataRequirementsTransactionBundle.json");
+		String bundleAsText = stringFromResource( "DataReqLibraryTransactionBundle.json");
 		Bundle bundle = (Bundle)getFhirContext().newJsonParser().parseResource(bundleAsText);
 		getClient().transaction().withBundle(bundle).execute();
 
