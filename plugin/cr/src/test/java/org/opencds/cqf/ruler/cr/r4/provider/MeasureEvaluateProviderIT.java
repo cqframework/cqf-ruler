5653a067bbcc8a91d4471183fbd862ccf80104de
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2022-03-07 08:34:34 -0700

    Added BCSE connectathon content as a measure test case

--- plugin/cr/src/test/java/org/opencds/cqf/ruler/cr/r4/provider/MeasureEvaluateProviderIT.java
+++ plugin/cr/src/test/java/org/opencds/cqf/ruler/cr/r4/provider/MeasureEvaluateProviderIT.java
@@ -3,7 +3,9 @@ package org.opencds.cqf.ruler.cr.r4.provider;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.MeasureReport;
+import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Parameters;
+import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.StringType;
 import org.junit.jupiter.api.Test;
 import org.opencds.cqf.ruler.cql.CqlConfig;
@@ -12,8 +14,7 @@ import org.opencds.cqf.ruler.devtools.DevToolsConfig;
 import org.opencds.cqf.ruler.test.RestIntegrationTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = { MeasureEvaluateProviderIT.class,
 		CrConfig.class, CqlConfig.class, DevToolsConfig.class }, properties = {
@@ -72,4 +73,65 @@ public class MeasureEvaluateProviderIT extends RestIntegrationTest {
 		assertNotNull(returnMeasureReport);
 	}
 
+	private void runWithPatient(String measureId, String patientId, int initialPopulationCount, int denominatorCount, int denominatorExclusionCount, int numeratorCount, boolean enrolledDuringParticipationPeriod, String participationPeriod) {
+		Parameters params = new Parameters();
+		params.addParameter().setName("periodStart").setValue(new StringType("2022-01-01"));
+		params.addParameter().setName("periodEnd").setValue(new StringType("2022-12-31"));
+		params.addParameter().setName("reportType").setValue(new StringType("individual"));
+		params.addParameter().setName("subject").setValue(new StringType(patientId));
+
+		MeasureReport  returnMeasureReport = getClient().operation().onInstance(new IdType("Measure", measureId))
+			.named("$evaluate-measure")
+			.withParameters(params)
+			.returnResourceType(MeasureReport.class)
+			.execute();
+
+		assertNotNull(returnMeasureReport);
+		//System.out.println("Resource:"+this.getFhirContext().newJsonParser().setPrettyPrint(true).encodeResourceToString(returnMeasureReport));
+
+		for (MeasureReport.MeasureReportGroupPopulationComponent population : returnMeasureReport.getGroupFirstRep().getPopulation()) {
+			switch (population.getCode().getCodingFirstRep().getCode()) {
+				case "initial-population": assertEquals(initialPopulationCount, population.getCount()); break;
+				case "denominator": assertEquals(denominatorCount, population.getCount()); break;
+				case "denominator-exclusion": assertEquals(denominatorExclusionCount, population.getCount()); break;
+				case "numerator": assertEquals(numeratorCount, population.getCount()); break;
+			}
+		}
+
+		Observation enrolledDuringParticipationPeriodObs = null;
+		Observation participationPeriodObs = null;
+		for (Resource r : returnMeasureReport.getContained()) {
+			if (r instanceof Observation) {
+				Observation o = (Observation)r;
+				if (o.getCode().getText().equals("Enrolled During Participation Period")) {
+					enrolledDuringParticipationPeriodObs = o;
+					continue;
+				}
+				else if (o.getCode().getText().equals("Participation Period")) {
+					participationPeriodObs = o;
+					continue;
+				}
+			}
+		}
+
+		assertNotNull(enrolledDuringParticipationPeriodObs);
+		assertTrue(enrolledDuringParticipationPeriodObs.getValueCodeableConcept().getCodingFirstRep().getCode().equals(Boolean.toString(enrolledDuringParticipationPeriod).toLowerCase()));
+
+		assertNotNull(participationPeriodObs);
+		assertTrue(participationPeriodObs.getValueCodeableConcept().getCodingFirstRep().getCode().equals(participationPeriod));
+	}
+
+	@Test
+	public void testBCSEHEDISMY2022() throws Exception {
+		String bundleAsText = stringFromResource( "BCSEHEDISMY2022-bundle.json");
+		Bundle bundle = (Bundle)getFhirContext().newJsonParser().parseResource(bundleAsText);
+		getClient().transaction().withBundle(bundle).execute();
+
+		runWithPatient("BCSEHEDISMY2022", "Patient/Patient-5", 0, 0, 0, 0, false, "Interval[2020-10-01T00:00:00.000, 2022-12-31T23:59:59.999]");
+		runWithPatient("BCSEHEDISMY2022", "Patient/Patient-7", 1, 1, 0, 0, true, "Interval[2020-10-01T00:00:00.000, 2022-12-31T23:59:59.999]");
+		runWithPatient("BCSEHEDISMY2022", "Patient/Patient-9", 0, 0, 0, 0, true, "Interval[2020-10-01T00:00:00.000, 2022-12-31T23:59:59.999]");
+		runWithPatient("BCSEHEDISMY2022", "Patient/Patient-21", 1, 0, 1, 0, true, "Interval[2020-10-01T00:00:00.000, 2022-12-31T23:59:59.999]");
+		runWithPatient("BCSEHEDISMY2022", "Patient/Patient-23", 1, 1, 0, 0, true, "Interval[2020-10-01T00:00:00.000, 2022-12-31T23:59:59.999]");
+		runWithPatient("BCSEHEDISMY2022", "Patient/Patient-65", 1, 1, 0, 1, true, "Interval[2020-10-01T00:00:00.000, 2022-12-31T23:59:59.999]");
+	}
 }
