c267797381295cbf9861cc169d47cacb06c8f1cc
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2022-06-03 09:05:57 -0600

    fix cds hooks tests fail

--- plugin/cds-hooks/src/test/java/org/opencds/cqf/ruler/plugin/cdshooks/r4/CdsHooksServletIT.java
+++ plugin/cds-hooks/src/test/java/org/opencds/cqf/ruler/plugin/cdshooks/r4/CdsHooksServletIT.java
@@ -32,7 +32,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { Application.class,
 		CdsHooksConfig.class }, properties = {
-				"hapi.fhir.fhir_version=r4",
+				"hapi.fhir.fhir_version=r4", "hapi.fhir.cr.security_configuration.enabled=false"
 		})
 public class CdsHooksServletIT extends RestIntegrationTest {
 	String ourCdsBase;
