c25ddb2cb561b0e1dac094811443a4b0df35cac6
rob-reynolds <rob@alphora.com>
2022-07-22 13:56:25 -0600

    Wip

--- plugin/security/src/main/java/org/opencds/cqf/ruler/security/interceptor/AuthenticationInterceptor.java
+++ plugin/security/src/main/java/org/opencds/cqf/ruler/security/interceptor/AuthenticationInterceptor.java
@@ -29,34 +29,36 @@ public class AuthenticationInterceptor implements org.opencds.cqf.ruler.api.Inte
 	@Hook(Pointcut.SERVER_INCOMING_REQUEST_POST_PROCESSED)
 	public boolean incomingRequestPostProcessed(RequestDetails theRequestDetails, HttpServletRequest theRequest,
 			HttpServletResponse theResponse) throws AuthenticationException {
-		if (securityProperties.getBasicAuth().isEnabled()) {
-			String authHeader = theRequest.getHeader("Authorization");
-
-			myLog.info("incoming request intercepted");
-			// The format of the header must be:
-			// Authorization: Basic [base64 of username:password]
-			if (authHeader == null || !authHeader.startsWith("Basic ")) {
-				throw new AuthenticationException("Missing or invalid Authorization header");
-			}
-
-			String base64 = authHeader.substring("Basic ".length());
-			String base64decoded = new String(Base64.getDecoder().decode(base64), UTF_8);
-			String[] parts = base64decoded.split(":");
-
-			if (parts.length <= 1) {
-				throw new AuthenticationException("Missing or invalid Authorization header");
-			}
-
-			String username = parts[0];
-			String password = parts[1];
-
-			if (!StringUtils.equals(username.trim(), securityProperties.getBasicAuth().getUsername().trim()) ||
-					!StringUtils.equals(password.trim(), securityProperties.getBasicAuth().getPassword().trim())) {
-				throw new AuthenticationException("Invalid username or password");
-			}
-
-			myLog.info("Authorization successful");
+		if (!securityProperties.getBasicAuth().isEnabled()) {
+			return true;
 		}
+
+		String authHeader = theRequest.getHeader("Authorization");
+
+		myLog.info("incoming request intercepted");
+		// The format of the header must be:
+		// Authorization: Basic [base64 of username:password]
+		if (authHeader == null || !authHeader.startsWith("Basic ")) {
+			throw new AuthenticationException("Missing or invalid Authorization header");
+		}
+
+		String base64 = authHeader.substring("Basic ".length());
+		String base64decoded = new String(Base64.getDecoder().decode(base64), UTF_8);
+		String[] parts = base64decoded.split(":");
+
+		if (parts.length <= 1) {
+			throw new AuthenticationException("Missing or invalid Authorization header");
+		}
+
+		String username = parts[0];
+		String password = parts[1];
+
+		if (!StringUtils.equals(username.trim(), securityProperties.getBasicAuth().getUsername().trim()) ||
+				!StringUtils.equals(password.trim(), securityProperties.getBasicAuth().getPassword().trim())) {
+			throw new AuthenticationException("Invalid username or password");
+		}
+
+		myLog.info("Authorization successful");
 
 		// Return true to allow the request to proceed
 		return true;
