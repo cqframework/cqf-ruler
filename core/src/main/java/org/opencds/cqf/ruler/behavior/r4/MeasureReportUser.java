693c4c9a0deb51680e677518dde9a619db5666c8
c-schuler <hoofschu@gmail.com>
2022-07-22 14:11:45 -0600

    #557 & #558 Added SDE to bundle entry for $care-gaps and $risk-adjustment (#567)
    * #558: added logic to include SDE resources in the bundle entry
    
    * #557: added logic to include SDE resources in the bundle entry
    
    * Update to auto formatting.
    
    * Fix bug in Thunder Client Care Gaps tests
    
    * Updated tests to validate evaluated resources and SDE
    
    * Update static extension url name to be clearer.  Update extension query to look for correct extension url
    
    Co-authored-by: rob-reynolds <rob@alphora.com>

--- core/src/main/java/org/opencds/cqf/ruler/behavior/r4/MeasureReportUser.java
+++ core/src/main/java/org/opencds/cqf/ruler/behavior/r4/MeasureReportUser.java
@@ -15,7 +15,11 @@ import org.hl7.fhir.r4.model.ContactPoint;
 import org.hl7.fhir.r4.model.ContactPoint.ContactPointSystem;
 import org.hl7.fhir.r4.model.Enumerations.PublicationStatus;
 import org.hl7.fhir.r4.model.Enumerations.SearchParamType;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.MeasureReport;
 import org.hl7.fhir.r4.model.OperationOutcome;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.SearchParameter;
 import org.hl7.fhir.r4.model.SearchParameter.XPathUsageType;
@@ -33,7 +37,7 @@ public interface MeasureReportUser extends DaoRegistryUser, IdCreator {
 
 	static final String MEASUREREPORT_IMPROVEMENT_NOTATION_SYSTEM = "http://terminology.hl7.org/CodeSystem/measure-improvement-notation";
 	static final String MEASUREREPORT_MEASURE_POPULATION_SYSTEM = "http://terminology.hl7.org/CodeSystem/measure-population";
-	static final String MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_SYSTEM = "http://hl7.org/fhir/us/davinci-deqm/StructureDefinition/extension-supplementalData";
+	static final String MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_EXTENSION = "http://hl7.org/fhir/us/davinci-deqm/StructureDefinition/extension-supplementalData";
 	static final String MEASUREREPORT_SUPPLEMENTALDATA_SEARCHPARAMETER_URL = "http://hl7.org/fhir/us/davinci-deqm/SearchParameter/measurereport-supplemental-data";
 	static final String MEASUREREPORT_SUPPLEMENTALDATA_SEARCHPARAMETER_VERSION = "0.1.0";
 
@@ -57,15 +61,14 @@ public interface MeasureReportUser extends DaoRegistryUser, IdCreator {
 		}
 	};
 
-	default Map<String, Resource> getEvaluatedResources(org.hl7.fhir.r4.model.MeasureReport report) {
+	default Map<String, Resource> getEvaluatedResources(MeasureReport report) {
 		Map<String, Resource> resources = new HashMap<>();
 		getEvaluatedResources(report, resources);
 
 		return resources;
 	}
 
-	default MeasureReportUser getEvaluatedResources(org.hl7.fhir.r4.model.MeasureReport report,
-			Map<String, Resource> resources) {
+	default MeasureReportUser getEvaluatedResources(MeasureReport report, Map<String, Resource> resources) {
 		report.getEvaluatedResource().forEach(evaluatedResource -> {
 			IIdType resourceId = evaluatedResource.getReferenceElement();
 			if (resourceId.getResourceType() == null || resources.containsKey(Ids.simple(resourceId))) {
@@ -78,6 +81,31 @@ public interface MeasureReportUser extends DaoRegistryUser, IdCreator {
 			}
 		});
 
+		return this;
+	}
+
+	default Map<String, Resource> getSDE(MeasureReport report) {
+		Map<String, Resource> sdeMap = new HashMap<>();
+		getSDE(report, sdeMap);
+		return sdeMap;
+	}
+
+	default MeasureReportUser getSDE(MeasureReport report, Map<String, Resource> resources) {
+		if (report.hasExtension()) {
+			for (Extension extension : report.getExtension()) {
+				if (extension.hasUrl() && extension.getUrl().equals(MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_EXTENSION)) {
+					Reference sdeRef = extension.hasValue() && extension.getValue() instanceof Reference
+							? (Reference) extension.getValue()
+							: null;
+					if (sdeRef != null && sdeRef.hasReference() && !sdeRef.getReference().startsWith("#")) {
+						IdType sdeId = new IdType(sdeRef.getReference());
+						if (!resources.containsKey(Ids.simple(sdeId))) {
+							resources.put(Ids.simple(sdeId), read(sdeId));
+						}
+					}
+				}
+			}
+		}
 		return this;
 	}
 
@@ -112,16 +140,17 @@ public interface MeasureReportUser extends DaoRegistryUser, IdCreator {
 				.setDescription(
 						String.format(
 								"Returns resources (supplemental data) from references on extensions on the MeasureReport with urls matching %s.",
-								MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_SYSTEM))
+								MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_EXTENSION))
 				.setJurisdiction(US_JURISDICTION_CODING)
 				.addBase("MeasureReport")
 				.setCode("supplemental-data")
 				.setType(SearchParamType.REFERENCE)
 				.setExpression(
-						String.format("MeasureReport.extension('%s').value", MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_SYSTEM))
+						String.format("MeasureReport.extension('%s').value",
+								MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_EXTENSION))
 				.setXpath(
 						String.format("f:MeasureReport/f:extension[@url='%s'].value",
-								MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_SYSTEM))
+								MEASUREREPORT_MEASURE_SUPPLEMENTALDATA_EXTENSION))
 				.setXpathUsage(XPathUsageType.NORMAL);
 
 		searchParameter.setId("deqm-measurereport-supplemental-data");
