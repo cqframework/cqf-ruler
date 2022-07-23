760e2b5bedca4bacaa6248014cb5655e35b40ccc
Rob Reynolds <rob@alphora.com>
2022-07-22 18:20:09 -0600

    Bug health check with security (#570)
    * Wip
    
    * Fix bug with kubernetes startup probe when simple authentication is enabled.
    
    * Update naming of probe variable to be deployment scenario generic
    
    * Update test name to match variable rename

--- plugin/security/src/main/java/org/opencds/cqf/ruler/security/interceptor/AuthenticationInterceptor.java
+++ plugin/security/src/main/java/org/opencds/cqf/ruler/security/interceptor/AuthenticationInterceptor.java
@@ -21,6 +21,7 @@ import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 
 @Interceptor
 public class AuthenticationInterceptor implements org.opencds.cqf.ruler.api.Interceptor {
+	private static final String METADATA_PATH = "/fhir/metadata";
 	private final Logger myLog = LoggerFactory.getLogger(AuthenticationInterceptor.class);
 
 	@Autowired
@@ -29,34 +30,40 @@ public class AuthenticationInterceptor implements org.opencds.cqf.ruler.api.Inte
 	@Hook(Pointcut.SERVER_INCOMING_REQUEST_POST_PROCESSED)
 	public boolean incomingRequestPostProcessed(RequestDetails theRequestDetails, HttpServletRequest theRequest,
 			HttpServletResponse theResponse) throws AuthenticationException {
-		if (securityProperties.getBasicAuth().isEnabled()) {
-			String authHeader = theRequest.getHeader("Authorization");
-
-			myLog.info("incoming request intercepted");
-			// The format of the header must be:
-			// Authorization: Basic [base64 of username:password]
-			if (authHeader == null || !authHeader.startsWith("Basic ")) {
-				throw new AuthenticationException("Missing or invalid Authorization header");
-			}
-
-			String base64 = authHeader.substring("Basic ".length());
-			String base64decoded = new String(Base64.getDecoder().decode(base64), UTF_8);
-			String[] parts = base64decoded.split(":");
-
-			if (parts.length <= 1) {
-				throw new AuthenticationException("Missing or invalid Authorization header");
-			}
-
-			String username = parts[0];
-			String password = parts[1];
-
-			if (!StringUtils.equals(username.trim(), securityProperties.getBasicAuth().getUsername().trim()) ||
-					!StringUtils.equals(password.trim(), securityProperties.getBasicAuth().getPassword().trim())) {
-				throw new AuthenticationException("Invalid username or password");
-			}
-
-			myLog.info("Authorization successful");
+		// The METADATA_PATH is used as a probe in various deployment scenarios and
+		// needs to be excluded to enable correct functionality of the probe(s) in the
+		// case where basic auth is enabled.
+		if (!securityProperties.getBasicAuth().isEnabled()
+				|| theRequest.getRequestURI().equals(METADATA_PATH)) {
+			return true;
 		}
+
+		String authHeader = theRequest.getHeader("Authorization");
+
+		myLog.info("incoming request intercepted");
+		// The format of the header must be:
+		// Authorization: Basic [base64 of username:password]
+		if (authHeader == null || !authHeader.startsWith("Basic ")) {
+			throw new AuthenticationException("Missing or invalid Authorization header");
+		}
+
+		String base64 = authHeader.substring("Basic ".length());
+		String base64decoded = new String(Base64.getDecoder().decode(base64), UTF_8);
+		String[] parts = base64decoded.split(":");
+
+		if (parts.length <= 1) {
+			throw new AuthenticationException("Missing or invalid Authorization header");
+		}
+
+		String username = parts[0];
+		String password = parts[1];
+
+		if (!StringUtils.equals(username.trim(), securityProperties.getBasicAuth().getUsername().trim()) ||
+				!StringUtils.equals(password.trim(), securityProperties.getBasicAuth().getPassword().trim())) {
+			throw new AuthenticationException("Invalid username or password");
+		}
+
+		myLog.info("Authorization successful");
 
 		// Return true to allow the request to proceed
 		return true;
--- plugin/security/src/test/java/org/opencds/cqf/ruler/security/interceptor/AuthenticatorInterceptorIT.java
+++ plugin/security/src/test/java/org/opencds/cqf/ruler/security/interceptor/AuthenticatorInterceptorIT.java
@@ -1,7 +1,11 @@
 package org.opencds.cqf.ruler.security.interceptor;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.hl7.fhir.r4.model.Bundle;
 import org.junit.jupiter.api.Test;
@@ -12,12 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {AuthenticatorInterceptorIT.class,
-	SecurityConfig.class, DevToolsConfig.class }, properties = {
-	"hapi.fhir.fhir_version=r4", "hapi.fhir.security.basic_auth.enabled=true",
-	"hapi.fhir.security.basic_auth.username=someuser",
-	"hapi.fhir.security.basic_auth.password=thepassword"
-})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {
+		AuthenticatorInterceptorIT.class,
+		SecurityConfig.class, DevToolsConfig.class }, properties = {
+				"hapi.fhir.fhir_version=r4", "hapi.fhir.security.basic_auth.enabled=true",
+				"hapi.fhir.security.basic_auth.username=someuser",
+				"hapi.fhir.security.basic_auth.password=thepassword"
+		})
 public class AuthenticatorInterceptorIT extends RestIntegrationTest {
 
 	@Test
@@ -25,8 +30,8 @@ public class AuthenticatorInterceptorIT extends RestIntegrationTest {
 		String bundleAsText = stringFromResource("test-bundle.json");
 		Bundle bundle = (Bundle) getFhirContext().newJsonParser().parseResource(bundleAsText);
 		getClient().transaction().withBundle(bundle)
-			.withAdditionalHeader("Authorization", "Basic c29tZXVzZXI6dGhlcGFzc3dvcmQ=")
-			.execute();
+				.withAdditionalHeader("Authorization", "Basic c29tZXVzZXI6dGhlcGFzc3dvcmQ=")
+				.execute();
 
 	}
 
@@ -49,10 +54,21 @@ public class AuthenticatorInterceptorIT extends RestIntegrationTest {
 
 		Exception ex = assertThrows(AuthenticationException.class, () -> {
 			getClient().transaction().withBundle(bundle)
-				.withAdditionalHeader("Authorization", "Basic blahblah")
-				.execute();
+					.withAdditionalHeader("Authorization", "Basic blahblah")
+					.execute();
 		});
 
 		assertEquals("HTTP 401 : Missing or invalid Authorization header", ex.getMessage());
+	}
+
+	@Test
+	public void testDeploymentProbeException() throws Exception {
+		String serverBase = getClient().getServerBase();
+		URL url = new URL(String.format("%smetadata", serverBase));
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		assertDoesNotThrow(() -> {
+			con.getResponseCode();
+		});
 	}
 }
