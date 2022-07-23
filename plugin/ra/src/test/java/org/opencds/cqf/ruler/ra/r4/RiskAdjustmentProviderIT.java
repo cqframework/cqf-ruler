f197c6ee86737c395454b828b69f9902ded428a1
rob-reynolds <rob@alphora.com>
2022-07-23 07:52:02 -0600

    Disable tests for RA plugin for known issue

--- plugin/ra/src/test/java/org/opencds/cqf/ruler/ra/r4/RiskAdjustmentProviderIT.java
+++ plugin/ra/src/test/java/org/opencds/cqf/ruler/ra/r4/RiskAdjustmentProviderIT.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opencds.cqf.ruler.utility.r4.Parameters.newParameters;
 import static org.opencds.cqf.ruler.utility.r4.Parameters.newPart;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.IdType;
@@ -13,6 +17,7 @@ import org.hl7.fhir.r4.model.MeasureReport;
 import org.hl7.fhir.r4.model.Parameters;
 import org.hl7.fhir.r4.model.Reference;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.opencds.cqf.ruler.behavior.r4.MeasureReportUser;
 import org.opencds.cqf.ruler.ra.RAConfig;
@@ -22,10 +27,6 @@ import org.opencds.cqf.ruler.test.utility.Urls;
 import org.opencds.cqf.ruler.utility.Ids;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { RiskAdjustmentProviderIT.class,
 		RAConfig.class }, properties = { "hapi.fhir.fhir_version=r4" })
@@ -41,6 +42,7 @@ class RiskAdjustmentProviderIT extends RestIntegrationTest {
 	}
 
 	@Test
+	@Disabled("Known issue: https://github.com/DBCG/cqf-ruler/issues/571")
 	void riskAssessmentHistoricOpen() {
 		loadTransaction("tests-hist-open-HCC189-bundle.json");
 
@@ -55,6 +57,7 @@ class RiskAdjustmentProviderIT extends RestIntegrationTest {
 	}
 
 	@Test
+	@Disabled("Known issue: https://github.com/DBCG/cqf-ruler/issues/571")
 	void riskAssessmentHistoricClosed() {
 		loadTransaction("tests-hist-closed-HCC189-bundle.json");
 
@@ -69,6 +72,7 @@ class RiskAdjustmentProviderIT extends RestIntegrationTest {
 	}
 
 	@Test
+	@Disabled("Known issue: https://github.com/DBCG/cqf-ruler/issues/571")
 	void riskAssessmentHistoricNetNew() {
 		loadTransaction("tests-netnew-HCC189-bundle.json");
 
@@ -84,19 +88,19 @@ class RiskAdjustmentProviderIT extends RestIntegrationTest {
 
 	private Parameters getRequestParameters(String subject) {
 		return newParameters(
-			newPart("periodStart", "2022-01-01"),
-			newPart("periodEnd", "2022-12-31"),
-			newPart("subject", subject),
-			newPart("type", "report"));
+				newPart("periodStart", "2022-01-01"),
+				newPart("periodEnd", "2022-12-31"),
+				newPart("subject", subject),
+				newPart("type", "report"));
 	}
 
 	private Parameters callOperation(Parameters requestParameters) {
 		return getClient().operation()
-			.onInstance(new IdType("Measure", "ConditionCategoryPOC"))
-			.named("$risk-adjustment")
-			.withParameters(requestParameters)
-			.returnResourceType(Parameters.class)
-			.execute();
+				.onInstance(new IdType("Measure", "ConditionCategoryPOC"))
+				.named("$risk-adjustment")
+				.withParameters(requestParameters)
+				.returnResourceType(Parameters.class)
+				.execute();
 	}
 
 	private void validateResultParameters(Parameters resultParams, String patientReference) {
@@ -111,17 +115,17 @@ class RiskAdjustmentProviderIT extends RestIntegrationTest {
 	private void validateBundle(Bundle riskAssessmentBundle) {
 		assertTrue(riskAssessmentBundle.hasEntry());
 		List<String> bundleResourceReferences = riskAssessmentBundle.getEntry().stream()
-			.map(entry -> entry.getResource().hasIdElement() ? Ids.simple(entry.getResource().getIdElement()) : null)
-			.collect(Collectors.toList());
+				.map(entry -> entry.getResource().hasIdElement() ? Ids.simple(entry.getResource().getIdElement()) : null)
+				.collect(Collectors.toList());
 		validateUniqueBundleEntry(bundleResourceReferences);
 		assertTrue(riskAssessmentBundle.getEntryFirstRep().getResource() instanceof MeasureReport);
 		MeasureReport raBundle = (MeasureReport) riskAssessmentBundle.getEntryFirstRep().getResource();
 		validateEvalResourcesInBundle(bundleResourceReferences, raBundle.getEvaluatedResource().stream()
-			.map(Reference::getReference).collect(Collectors.toList()));
+				.map(Reference::getReference).collect(Collectors.toList()));
 		validateSdeInBundle(bundleResourceReferences, raBundle.getExtension().stream()
-			.filter(extension -> extension.getUrl().equals(
-				MeasureReportUser.MEASUREREPORT_SUPPLEMENTALDATA_SEARCHPARAMETER_URL))
-			.map(extension -> extension.getValue().toString()).collect(Collectors.toList()));
+				.filter(extension -> extension.getUrl().equals(
+						MeasureReportUser.MEASUREREPORT_SUPPLEMENTALDATA_SEARCHPARAMETER_URL))
+				.map(extension -> extension.getValue().toString()).collect(Collectors.toList()));
 	}
 
 	private void validateUniqueBundleEntry(List<String> entryReferences) {
@@ -137,41 +141,41 @@ class RiskAdjustmentProviderIT extends RestIntegrationTest {
 	}
 
 	private void validateMeasureReport(MeasureReport response, String suspectType,
-												  String evidenceStatus, String evidenceStatusDate) {
+			String evidenceStatus, String evidenceStatusDate) {
 		assertFalse(response.getGroup().isEmpty());
 		assertEquals(1, response.getGroup().size());
 		assertFalse(response.getGroupFirstRep().getExtension().isEmpty());
 		assertEquals(3, response.getGroupFirstRep().getExtension().size());
 		assertTrue(response.getGroupFirstRep().getExtensionFirstRep().hasValue());
 		assertTrue(response.getGroupFirstRep().getExtensionFirstRep().getValue() instanceof CodeableConcept
-			&& ((CodeableConcept) response.getGroupFirstRep().getExtensionFirstRep().getValue()).hasCoding());
+				&& ((CodeableConcept) response.getGroupFirstRep().getExtensionFirstRep().getValue()).hasCoding());
 		assertEquals("http://hl7.org/fhir/us/davinci-ra/StructureDefinition/ra-suspectType",
-			((CodeableConcept) response.getGroupFirstRep().getExtensionFirstRep().getValue()).getCodingFirstRep()
-				.getSystem());
+				((CodeableConcept) response.getGroupFirstRep().getExtensionFirstRep().getValue()).getCodingFirstRep()
+						.getSystem());
 		assertEquals(suspectType, ((CodeableConcept) response.getGroupFirstRep().getExtensionFirstRep().getValue())
-			.getCodingFirstRep().getCode());
+				.getCodingFirstRep().getCode());
 		assertTrue(response.getGroupFirstRep().getExtension().get(1).getValue() instanceof CodeableConcept
-			&& ((CodeableConcept) response.getGroupFirstRep().getExtension().get(1).getValue()).hasCoding());
+				&& ((CodeableConcept) response.getGroupFirstRep().getExtension().get(1).getValue()).hasCoding());
 		assertEquals("http://hl7.org/fhir/us/davinci-ra/StructureDefinition/ra-evidenceStatus",
-			((CodeableConcept) response.getGroupFirstRep().getExtension().get(1).getValue()).getCodingFirstRep()
-				.getSystem());
+				((CodeableConcept) response.getGroupFirstRep().getExtension().get(1).getValue()).getCodingFirstRep()
+						.getSystem());
 		assertEquals(evidenceStatus, ((CodeableConcept) response.getGroupFirstRep().getExtension().get(1).getValue())
-			.getCodingFirstRep().getCode());
+				.getCodingFirstRep().getCode());
 		assertTrue(response.getGroupFirstRep().getExtension().get(2).getValue() instanceof CodeableConcept
-			&& ((CodeableConcept) response.getGroupFirstRep().getExtension().get(2).getValue()).hasCoding());
+				&& ((CodeableConcept) response.getGroupFirstRep().getExtension().get(2).getValue()).hasCoding());
 		assertEquals("http://hl7.org/fhir/us/davinci-ra/StructureDefinition/ra-evidenceStatusDate",
-			((CodeableConcept) response.getGroupFirstRep().getExtension().get(2).getValue()).getCodingFirstRep()
-				.getSystem());
+				((CodeableConcept) response.getGroupFirstRep().getExtension().get(2).getValue()).getCodingFirstRep()
+						.getSystem());
 		assertEquals(evidenceStatusDate, ((CodeableConcept) response.getGroupFirstRep().getExtension().get(2).getValue())
-			.getCodingFirstRep().getCode());
+				.getCodingFirstRep().getCode());
 		assertTrue(response.getGroupFirstRep().hasCode() && response.getGroupFirstRep().getCode().hasCoding());
 		assertTrue(response.getGroupFirstRep().getCode().getCodingFirstRep().hasSystem());
 		assertEquals("http://terminology.hl7.org/CodeSystem/cmshcc",
-			response.getGroupFirstRep().getCode().getCodingFirstRep().getSystem());
+				response.getGroupFirstRep().getCode().getCodingFirstRep().getSystem());
 		assertTrue(response.getGroupFirstRep().getCode().getCodingFirstRep().hasCode());
 		assertEquals("189", response.getGroupFirstRep().getCode().getCodingFirstRep().getCode());
 		assertTrue(response.getGroupFirstRep().getCode().getCodingFirstRep().hasDisplay());
 		assertEquals("Amputation Status, Lower Limb/Amputation Complications",
-			response.getGroupFirstRep().getCode().getCodingFirstRep().getDisplay());
+				response.getGroupFirstRep().getCode().getCodingFirstRep().getDisplay());
 	}
 }
