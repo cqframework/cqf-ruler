e77a9968bef259b9da769e194aaaa35012be7644
Hayes Murdock <hayes@alphora.com>
2022-09-15 14:38:12 -0600

    APHL-303 Updates for backend for release and publish operations

--- plugin/cql/src/main/java/org/opencds/cqf/ruler/cql/KnowledgeArtifactProcessor.java
+++ plugin/cql/src/main/java/org/opencds/cqf/ruler/cql/KnowledgeArtifactProcessor.java
@@ -3,11 +3,16 @@ package org.opencds.cqf.ruler.cql;
 import ca.uhn.fhir.model.api.IQueryParameterType;
 import ca.uhn.fhir.rest.param.UriParam;
 import org.cqframework.fhir.api.FhirDal;
+import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.CanonicalType;
 import org.hl7.fhir.r4.model.Enumerations;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Library;
 import org.hl7.fhir.r4.model.MetadataResource;
 import org.hl7.fhir.r4.model.RelatedArtifact;
+import org.hl7.fhir.r4.model.Resource;
+import org.opencds.cqf.ruler.provider.DaoRegistryOperationProvider;
 import org.opencds.cqf.ruler.utility.Canonicals;
 
 import java.util.ArrayList;
@@ -16,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 // TODO: This belongs in the Evaluator. Only included in Ruler at dev time for shorter cycle.
-public class KnowledgeArtifactProcessor {
+public class KnowledgeArtifactProcessor  extends DaoRegistryOperationProvider {
 	// TODO: Autowire?
 	// private KnowledgeArtifactAdapter adapter;
 
@@ -59,5 +64,67 @@ public class KnowledgeArtifactProcessor {
 		}
 
 		return newResource;
+	}
+
+	public MetadataResource releaseVersion(IdType iIdType, FhirDal fhirDal) {
+
+		MetadataResource resource = read(iIdType);
+		String resourceVersionId = resource.getMeta().getVersionId();
+		if(!resourceVersionId.contains("draft")) {
+			throw new FHIRException("Resource found by " + iIdType + " is not a draft version.  Please select a draft resource.");
+		}
+		if(!resourceVersionId.startsWith("draft")) {
+			throw new FHIRException("Resource found by " + iIdType + " is not a draft version.  The version does not start with draft.");
+		}
+		String[] resourceIdArray = resourceVersionId.split("draft-");
+		String updatedResourceId = resourceIdArray[1];
+		resource.setId(updatedResourceId);
+
+		Bundle bundle = new Bundle();
+		bundle.getEntry().forEach(
+			entry -> {
+				if(entry.hasResource() && entry.getResource() instanceof Library) {
+					Enumerations.PublicationStatus status = ((Library) entry.getResource()).getStatus();
+					if(!status.equals(Enumerations.PublicationStatus.ACTIVE)) {
+						((Library) entry.getResource()).setStatus(Enumerations.PublicationStatus.ACTIVE);
+					}
+				}
+			}
+		);
+
+		update(resource);
+
+		return resource;
+
+	}
+
+	public MetadataResource publishVersion(IdType iIdType, FhirDal fhirDal) {
+
+		MetadataResource resource = read(iIdType);
+		String resourceVersionId = resource.getMeta().getVersionId();
+		if(!resourceVersionId.contains("draft")) {
+			throw new FHIRException("Resource found by " + iIdType + " is not a draft version.  Please select a draft resource.");
+		}
+		if(!resourceVersionId.startsWith("draft")) {
+			throw new FHIRException("Resource found by " + iIdType + " is not a draft version.  The version does not start with draft.");
+		}
+		String[] resourceIdArray = resourceVersionId.split("draft-");
+		String updatedResourceId = resourceIdArray[1];
+		resource.setId(updatedResourceId);
+
+		Bundle bundle = new Bundle();
+		bundle.getEntry().forEach(
+			entry -> {
+				if(entry.hasResource() && entry.getResource() instanceof Library) {
+					Enumerations.PublicationStatus status = ((Library) entry.getResource()).getStatus();
+					if(status.equals(Enumerations.PublicationStatus.ACTIVE)) {
+						create(resource);
+					}
+				}
+			}
+		);
+
+		return resource;
+
 	}
 }
--- plugin/cql/src/main/java/org/opencds/cqf/ruler/cql/RepositoryService.java
+++ plugin/cql/src/main/java/org/opencds/cqf/ruler/cql/RepositoryService.java
@@ -6,7 +6,9 @@ import ca.uhn.fhir.rest.annotation.OperationParam;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import org.cqframework.fhir.api.FhirDal;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Library;
+import org.hl7.fhir.r4.model.Resource;
 import org.opencds.cqf.ruler.provider.DaoRegistryOperationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,6 +20,9 @@ public class RepositoryService extends DaoRegistryOperationProvider {
 	@Autowired
 	private JpaFhirDalFactory fhirDalFactory;
 
+	@Autowired
+		private KnowledgeArtifactProcessor artifactProcessor;
+
 	 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
 
     public RepositoryService() {}
@@ -25,12 +30,34 @@ public class RepositoryService extends DaoRegistryOperationProvider {
 	 @Operation(name = "$draft", idempotent = true, type = Library.class)
 	 @Description(shortDefinition = "$draft", value = "Create a new draft library version")
 	 public Library draftOperation(RequestDetails requestDetails, @OperationParam(name = "currentLibrary") Library currentLibrary) throws FHIRException {
-		FhirDal fhirDal = (FhirDal) this.fhirDalFactory.create(requestDetails);
 
-		KnowledgeArtifactProcessor processor = new KnowledgeArtifactProcessor();
-		//TODO: Autowire the processor?
-		 Library draftLibrary = (Library)processor.newVersion(currentLibrary, fhirDal);
+		 FhirDal fhirDal = (FhirDal) this.fhirDalFactory.create(requestDetails);
+
+		 Library draftLibrary = (Library)this.artifactProcessor.newVersion(currentLibrary, fhirDal);
 
 		return draftLibrary;
 	 }
+
+	@Operation(name = "$release", idempotent = true, type = Resource.class)
+	@Description(shortDefinition = "$release", value = "Update an existing draft artifact to active")
+	public Resource releaseOperation(RequestDetails requestDetails, @OperationParam(name="iIdType") IdType iIdType) throws FHIRException {
+
+		FhirDal fhirDal = (FhirDal) this.fhirDalFactory.create(requestDetails);
+
+		Library draftLibrary = (Library) this.artifactProcessor.releaseVersion(iIdType, fhirDal);
+
+		return draftLibrary;
+	}
+
+	@Operation(name = "$publish", idempotent = true, type = Resource.class)
+	@Description(shortDefinition = "$publish", value = "Post a new artifact with active status")
+	public Resource publishVersion(RequestDetails requestDetails, @OperationParam(name = "iIdType") IdType iIdType) throws FHIRException {
+
+		FhirDal fhirDal = (FhirDal) this.fhirDalFactory.create(requestDetails);
+
+		Library draftLibrary = (Library) this.artifactProcessor.publishVersion(iIdType, fhirDal);
+
+		return draftLibrary;
+
+	}
 }
