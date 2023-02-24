1de04e96692a384ed076bb38cf83f75734e4192a
Adam Stevenson <stevenson_adam@yahoo.com>
2023-02-24 09:21:15 -0700

    Removed unused import.

--- plugin/cql/src/main/java/org/opencds/cqf/ruler/cql/KnowledgeArtifactProcessor.java
+++ plugin/cql/src/main/java/org/opencds/cqf/ruler/cql/KnowledgeArtifactProcessor.java
@@ -26,7 +26,6 @@ import org.springframework.beans.factory.annotation.Configurable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
