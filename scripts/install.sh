141a557ba388aebc3b920ca7404c65da377f9880
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-11-27 14:33:56 -0500

    update scripts and yaml

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,8 @@
 # Fail if any command fails or if there are unbound variables and check syntax
 set -euxo pipefail
 bash -n "$0"
-
+# install base
 mvn install -U -DskipTests=true -Dmaven.javadoc.skip=true -T 4 -B -V
+# install plugin
+cd ./ecr
+mvn package -U -DskipTests=true -Dmaven.javadoc.skip=true -T 4 -B -V
