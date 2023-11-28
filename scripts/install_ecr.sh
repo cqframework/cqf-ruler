f71e0db997b4e1e406ef8f789bccd331b91fdf06
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-11-28 12:59:52 -0500

    test

--- scripts/install_ecr.sh
+++ scripts/install_ecr.sh
@@ -3,4 +3,5 @@
 # Fail if any command fails or if there are unbound variables and check syntax
 set -euxo pipefail
 bash -n "$0"
+cd ./ecr
 mvn install -U -DskipTests=true -Dmaven.javadoc.skip=true -T 4 -B -V
