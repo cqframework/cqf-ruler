176feb21e814f33c6f0a3151a7e9c6d30fd51967
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-12-22 16:35:46 -0500

    [APHL-300] update interceptor

--- plugin/cr/src/main/java/org/opencds/cqf/ruler/cr/ValueSetInterceptor.java
+++ plugin/cr/src/main/java/org/opencds/cqf/ruler/cr/ValueSetInterceptor.java
@@ -5,12 +5,9 @@ import java.util.List;
 
 import org.cqframework.fhir.api.FhirDal;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
-import org.hl7.fhir.r4.model.Bundle.BundleEntryRequestComponent;
 import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Library;
+import org.hl7.fhir.r4.model.RelatedArtifact;
 import org.hl7.fhir.r4.model.ResourceType;
 import org.hl7.fhir.r4.model.ValueSet;
 import org.hl7.fhir.r4.model.ValueSet.ConceptReferenceComponent;
@@ -40,77 +37,52 @@ public class ValueSetInterceptor implements org.opencds.cqf.ruler.api.Intercepto
    public void update(
     IBaseResource theOldResource,
     IBaseResource theResource,
-    RequestDetails theRequestDetails) {
+    RequestDetails theRequestDetails) throws UnprocessableEntityException {
       if (theResource.fhirType().equals(ResourceType.ValueSet.toString())) {
         ValueSet vs = (ValueSet) theResource;
-        FhirDal fhirDal = new JpaFhirDal(myDaoRegistry,theRequestDetails);
         if (vs.getUrl().equals("http://ersd.aimsplatform.org/fhir/ValueSet/rckms-condition-codes")) {
-          // for each code in the value set we need to pull all the libraries which reference it
-          // once we have all the libraries we need to check every related artifact and update the VSCondition URLs accordingly
+          FhirDal fhirDal = new JpaFhirDal(myDaoRegistry,theRequestDetails);
           List<ConceptSetComponent> sets = vs.getCompose().getInclude();
-          Bundle transactionBundle = new Bundle().setType(Bundle.BundleType.TRANSACTION);
           List<Library> librariesToUpdate = new ArrayList<Library>();
           for (ConceptSetComponent set: sets) {
+            // for each system in the value set
             String system = set.getSystem();
             List<ConceptReferenceComponent> concepts = set.getConcept();
-            // for each concept.code we need to update the extension.valueCodeableConcept.coding.display to concept.designation(where code = synonym).value
             for (ConceptReferenceComponent concept:concepts) {
-              SearchParameterMap map = new SearchParameterMap();
+              // for each concept.code need to update the extension.valueCodeableConcept.coding.display to concept.designation(where code = synonym).value
               TokenParam token = new TokenParam(system,concept.getCode());
-              map.add("manifest-contains-code",token);
+              SearchParameterMap map = new SearchParameterMap("manifest-contains-code",token);
               TypedBundleProvider<Library> result = search(Library.class, map);
               result.getAllResourcesTyped().stream().forEach(lib -> {
-                Library addToListIfMissing = librariesToUpdate
+                Library getCachedLibraryOrAdd = librariesToUpdate
                   .stream().filter(existing -> existing.getId().equals(lib.getId())).findFirst()
                   .orElseGet(() -> {
                     librariesToUpdate.add(lib);
                     return lib;
                   });
                 String newText = concept.getDesignation()
-                  .stream().filter(designation -> designation.getUse().getCode().equals("synonym")).findFirst().orElseThrow(()-> new UnprocessableEntityException("No synonym found!"))
+                  .stream().filter(designation -> designation.getUse().getCode().equals("synonym"))
+                  .findFirst().orElseThrow(()-> new UnprocessableEntityException("No synonym found for code: " + system + "|" + concept.getCode()))
                   .getValue();
-                updateConditionWithNewValue(addToListIfMissing,system, concept.getCode(),newText );
+                updateConditionWithNewSynonym(getCachedLibraryOrAdd,system, concept.getCode(),newText );
               });
-              // transactionBundle.addEntry(createEntry(system, concept.getCode()));
             }
           }
           for (Library library:librariesToUpdate) {
             fhirDal.update(library);
           }
-          
-          // create a Transaction search'
-          // Bundle transactionBundle = new Bundle().setType(Bundle.BundleType.TRANSACTION);
-          // Bundle result = transaction(transactionBundle);
-          // if(result.getEntry().size() > 0){
-          //   var zeo = result.getEntry().get(0);
-          //   zeo.getResource();
-          // }
         }
       }
    }
-   private void updateConditionWithNewValue(Library library, String system, String code, String newText) {
-    List<Extension> extensions = library.getExtension();
-    extensions.stream()
-      .filter(ext -> ext.getUrl().equals(KnowledgeArtifactProcessor.valueSetConditionUrl))
+   private void updateConditionWithNewSynonym(Library library, String system, String code, String newText) {
+    List<RelatedArtifact> relatedArtifacts = library.getRelatedArtifact();
+    relatedArtifacts.stream()
+      .map(ra -> ra.getExtensionByUrl(KnowledgeArtifactProcessor.valueSetConditionUrl))
+      .filter(ext -> ext != null)
       .map(ext -> (CodeableConcept)ext.getValue())
       .filter(concept -> concept.getCoding().stream().anyMatch(coding -> coding.getSystem().equals(system) && coding.getCode().equals(code)))
       .forEach(concept -> {
         concept.setText(newText);
       });
    }
-  private BundleEntryComponent createEntry(String system, String code) {
-		BundleEntryComponent entry = new Bundle.BundleEntryComponent()
-				.setRequest(createRequest(system + "|" + code));
-		return entry;
-	}
-
-	private BundleEntryRequestComponent createRequest(String token) {
-		Bundle.BundleEntryRequestComponent request = new Bundle.BundleEntryRequestComponent();
-    String query = "Library?manifest-contains-code=" + token;
-    request
-      .setMethod(Bundle.HTTPVerb.GET)
-      .setUrl(query);
-		return request;
-	}
-
 }
