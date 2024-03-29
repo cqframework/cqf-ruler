f5f932de91def1fffdc5ea2ca03165f21808df9a
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-03-29 12:21:12 -0600

    More dependency cleanup

--- plugin/cds-hooks/src/main/java/org/opencds/cqf/ruler/cdshooks/CdsHooksConfig.java
+++ plugin/cds-hooks/src/main/java/org/opencds/cqf/ruler/cdshooks/CdsHooksConfig.java
@@ -10,7 +10,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.jpa.cache.IResourceChangeListenerRegistry;
@@ -42,24 +41,30 @@ public class CdsHooksConfig {
 		return listener;
 	}
 	/*
-	@Bean
-	@Conditional(OnDSTU3Condition.class)
-	@DependsOn({ "dstu3CqlExecutionProvider", "dstu3LibraryEvaluationProvider" })
-	public ServletRegistrationBean<org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet> cdsHooksRegistrationBeanDstu3() {
-		org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet cdsHooksServlet = new org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet();
-		beanFactory.autowireBean(cdsHooksServlet);
-
-		ServletRegistrationBean<org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet> registrationBean = new ServletRegistrationBean<>();
-		registrationBean.setName("cds-hooks servlet");
-		registrationBean.setServlet(cdsHooksServlet);
-		registrationBean.addUrlMappings("/cds-services/*");
-		registrationBean.setLoadOnStartup(1);
-		return registrationBean;
-	}*/
+	 * @Bean
+	 * 
+	 * @Conditional(OnDSTU3Condition.class)
+	 * 
+	 * @DependsOn({ "dstu3CqlExecutionProvider", "dstu3LibraryEvaluationProvider" })
+	 * public
+	 * ServletRegistrationBean<org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet>
+	 * cdsHooksRegistrationBeanDstu3() {
+	 * org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet cdsHooksServlet = new
+	 * org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet();
+	 * beanFactory.autowireBean(cdsHooksServlet);
+	 * 
+	 * ServletRegistrationBean<org.opencds.cqf.ruler.cdshooks.dstu3.CdsHooksServlet>
+	 * registrationBean = new ServletRegistrationBean<>();
+	 * registrationBean.setName("cds-hooks servlet");
+	 * registrationBean.setServlet(cdsHooksServlet);
+	 * registrationBean.addUrlMappings("/cds-services/*");
+	 * registrationBean.setLoadOnStartup(1);
+	 * return registrationBean;
+	 * }
+	 */
 
 	@Bean
 	@Conditional(OnR4Condition.class)
-	@DependsOn({ "r4CqlExecutionProvider", "r4LibraryEvaluationProvider" })
 	public ServletRegistrationBean<org.opencds.cqf.ruler.cdshooks.r4.CdsHooksServlet> cdsHooksRegistrationBeanR4() {
 		org.opencds.cqf.ruler.cdshooks.r4.CdsHooksServlet cdsHooksServlet = new org.opencds.cqf.ruler.cdshooks.r4.CdsHooksServlet();
 		beanFactory.autowireBean(cdsHooksServlet);
