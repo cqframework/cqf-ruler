ffb6ba3835439454a74d416d661ccb53021e3dcb
Brenin Rhodes <brenin@alphora.com>
2024-04-04 10:15:44 -0600

    Remove extract provider

--- plugin/sdc/src/main/java/org/opencds/cqf/ruler/sdc/SDCProviderFactory.java
+++ plugin/sdc/src/main/java/org/opencds/cqf/ruler/sdc/SDCProviderFactory.java
@@ -1,6 +1,5 @@
 package org.opencds.cqf.ruler.sdc;
 
-import org.opencds.cqf.ruler.sdc.dstu3.ExtractProvider;
 import org.opencds.cqf.ruler.sdc.dstu3.TransformProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -14,19 +13,6 @@ public class SDCProviderFactory {
 
 	@Autowired
 	private ApplicationContext myApplicationContext;
-
-	public Object getExtractProvider() {
-		switch (myFhirContext.getVersion().getVersion()) {
-			case DSTU3:
-				return myApplicationContext.getBean(ExtractProvider.class);
-			case R4:
-				return myApplicationContext
-					.getBean(org.opencds.cqf.ruler.sdc.r4.ExtractProvider.class);
-			default:
-				throw new ConfigurationException("ExtractProvider not supported for FHIR version "
-					+ myFhirContext.getVersion().getVersion());
-		}
-	}
 
 	public Object getTransformProvider() {
 		switch (myFhirContext.getVersion().getVersion()) {
